ce15b9d7c5e5551b33398b3b6ffe9501e4000ae3
pulmonarytoolkit <pulmonarytoolkit@gmail.com>
2012-03-15 15:55:14 +0000

    Added new plugin for visualising airways and lung volumes together

---
+++ Plugins/TDAirwaysAndLungs.m
@@ -0,0 +1,57 @@
+classdef TDAirwaysAndLungs < TDPlugin
+    % TDAirwaysAndLungs. Plugin for displaying segented airways and lungs together
+    %
+    %     This is a plugin for the Pulmonary Toolkit. Plugins can be run using 
+    %     the gui, or through the interfaces provided by the Pulmonary Toolkit.
+    %     See TDPlugin.m for more information on how to run plugins.
+    %
+    %     Plugins should not be run directly from your code.
+    %
+    %
+    %
+    %     Licence
+    %     -------
+    %     Part of the TD Pulmonary Toolkit. http://code.google.com/p/pulmonarytoolkit
+    %     Author: Tom Doel, 2012.  www.tomdoel.com
+    %     Distributed under the GNU GPL v3 licence. Please see website for details.
+    %    
+    
+    properties
+        ButtonText = 'Airways and Lungs'
+        ToolTip = 'Shows a segmentation of the airways and lungs'
+        Category = 'Airways'
+
+        AllowResultsToBeCached = true
+        AlwaysRunPlugin = true
+        PluginType = 'ReplaceOverlay'
+        HidePluginInDisplay = false
+        FlattenPreviewImage = true
+        TDPTKVersion = '1'
+        ButtonWidth = 6
+        ButtonHeight = 2
+        GeneratePreview = true
+    end
+    
+    methods (Static)
+        function results = RunPlugin(dataset, reporting)
+            
+            % Get the airway results
+            [airways_results, airways_image] = dataset.GetResult('TDAirways');
+            
+            % Get the results for the left and right lung segmentations
+            [left_and_right_lungs_results, left_and_right_lungs_image] = dataset.GetResult('TDLeftAndRightLungs');
+            
+            % Make our output image the lugn segmentation
+            results = left_and_right_lungs_image.BlankCopy;
+            
+            % Make an image matrix with 1 for right lung, 2 for left lung and 3
+            % for airways
+            combined_image_raw = left_and_right_lungs_image.RawImage;
+            combined_image_raw(airways_image.RawImage == 1) = 3;
+            
+            % Change the output image
+            results.ChangeRawImage(combined_image_raw);
+            
+        end        
+    end
+end
