1681fcae2b98080d580c7f51580087475639600c
pulmonarytoolkit <pulmonarytoolkit@gmail.com>
2012-05-09 19:58:14 +0000

    TDGetMainRegionExcludingBorder  - Added checks to deal with the case of there being only one major connected component.  - Added some leeway in the left/right lung separation for MRI images, to deal with cases where MRI segmentation is poor.

--- Library/TDGetMainRegionExcludingBorder.m
+++ Library/TDGetMainRegionExcludingBorder.m
@@ -52,7 +52,7 @@ function threshold_image = TDGetMainRegionExcludingBorder(threshold_image, repor
     result_index = 1;
     results = [];
     index_in_sorted_array = 1;
-    while (result_index < 3 && index_in_sorted_array < length(sorted_largest_areas_indices))
+    while (result_index < 3 && index_in_sorted_array <= length(sorted_largest_areas_indices))
         current_region_being_checked = sorted_largest_areas_indices(index_in_sorted_array);
         bordered_image(:) = false;
         bordered_image(CC.PixelIdxList{current_region_being_checked}) = true;
@@ -66,20 +66,39 @@ function threshold_image = TDGetMainRegionExcludingBorder(threshold_image, repor
         index_in_sorted_array = index_in_sorted_array + 1;
     end
     
-    bounding_boxes = regionprops(CC, 'BoundingBox');
-    bb_1 = bounding_boxes(results(1)).BoundingBox;
-    bb_2 = bounding_boxes(results(2)).BoundingBox;
-    image_centre = round(size(bordered_image)/2);
     
-    use_both_regions = false;
-    if bb_1(1) >= image_centre(1) && bb_2(4) < image_centre(1)
-        reporting.ShowMessage('I appear to have found 2 disconnected lungs. I am connecting them.');        
-        use_both_regions = true;
-    end
+    if numel(results) > 1
+        % If more than one region was found (after excluding boundary-touching
+        % regions), then check if they are disconnected left and right lungs
+        bounding_boxes = regionprops(CC, 'BoundingBox');
+        bb_1 = bounding_boxes(results(1)).BoundingBox;
+        bb_1 = bb_1([2,1,3,5,4,6]); % Bounding box is [xyz] but Matlab indices are [yxz]
+        bb_2 = bounding_boxes(results(2)).BoundingBox;
+        bb_2 = bb_2([2,1,3,5,4,6]); % Bounding box is [xyz] but Matlab indices are [yxz]
+        image_centre = round(size(bordered_image)/2);
         
-    if bb_2(1) >= image_centre(1) && bb_1(4) < image_centre(1)
-        reporting.ShowMessage('I appear to have found 2 disconnected lungs. I am connecting them.');        
-        use_both_regions = true;
+        image_centre_j = image_centre(2);
+        
+        if threshold_image.IsCT
+            centre_offset = 0;
+        else
+            % Allow some leeway for MRI images where the lung boundaries may not be so
+            % clear
+            centre_offset = 10;
+        end
+        
+        use_both_regions = false;
+        if (bb_1(2) >= image_centre_j - centre_offset) && (bb_2(5) < image_centre_j + centre_offset)
+            reporting.ShowMessage('I appear to have found 2 disconnected lungs. I am connecting them.');
+            use_both_regions = true;
+        end
+        
+        if (bb_2(2) >= image_centre_j - centre_offset) && (bb_1(5) < image_centre_j + centre_offset)
+            reporting.ShowMessage('I appear to have found 2 disconnected lungs. I am connecting them.');
+            use_both_regions = true;
+        end
+    else
+        use_both_regions = false;
     end
 
     bordered_image(:) = false;
