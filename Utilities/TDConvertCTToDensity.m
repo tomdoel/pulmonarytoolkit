c9c330c732d083711fefc673325e6de7a9ac1367
pulmonarytoolkit <pulmonarytoolkit@gmail.com>
2012-07-14 22:24:30 +0000

    TDConvertCTToDensity  - Slightly modified the conversion of Hounsfield Unit to mass density so that it assumes a linear scale calibrated at water and air values, and sets a minimum at zero (since this conversion is only an approximation, the resulting density values can be less than zero, so we prevent this).

--- Utilities/TDConvertCTToDensity.m
+++ Utilities/TDConvertCTToDensity.m
@@ -7,8 +7,20 @@ function density_image = TDConvertCTToDensity(ct_image)
     % Convert to HU
     density_values_raw = double(ct_image.GreyscaleToHounsfield(double(density_values_raw)));
     
+    density_water_mgmL = 1000;
+    density_air_stp_mgmL = 1.2922;
+    HU_air = -1000;
+    
     % Convert to density mg/mL
-    density_values_raw = density_values_raw + 1000;
+    % We are assuming a linear relationship between radiodensity and mass
+    % density - in relaity this relationship depends on the material being
+    % scanned and the calibration of the scanner
+    alpha = (density_air_stp_mgmL - density_water_mgmL)/HU_air;
+    density_values_raw = alpha*density_values_raw + density_water_mgmL;
+    
+    % Because this linear relationship is an approximation, we might get
+    % negative density values - threshold at zero
+    density_values_raw = max(0, density_values_raw);
 
     density_image.ChangeRawImage(density_values_raw);
     density_image.ImageType = TDImageType.Grayscale;
