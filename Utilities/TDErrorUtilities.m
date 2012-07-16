db52d05aa5032cc54c3377570802c3e1293ff2c2
pulmonarytoolkit <pulmonarytoolkit@gmail.com>
2012-07-16 23:28:12 +0000

    Utilities/TDErrorUtilities  - updated GetCallingFunction() so that any calling functions with prefix names 'TDReportingxxx' are excluded from the function name search

--- Utilities/TDErrorUtilities.m
+++ Utilities/TDErrorUtilities.m
@@ -14,12 +14,20 @@ classdef TDErrorUtilities < handle
         % Obtain the name of the function which signalled the error (useful in
         % error reporting)
         function [calling_function, stack] = GetCallingFunction(levels_to_ignore)
+            caller_found = false;
+            max_levels = 10;
             
-            % Get the call stack, excluding the call to this function and the
-            % caller of this function
-            [stack, ~] = dbstack(levels_to_ignore, '-completenames');
-            stack_top = stack(1);
-            calling_function = stack_top.name;
+            while ~caller_found
+                % Get the call stack, excluding the call to this function and the
+                % caller of this function
+                [stack, ~] = dbstack(levels_to_ignore, '-completenames');
+                stack_top = stack(1);
+                calling_function = stack_top.name;
+                if length(calling_function) < 11 || ~strcmp(calling_function(1:11), 'TDReporting')
+                    caller_found = true;
+                end
+                levels_to_ignore = levels_to_ignore + 1;
+            end
         end
     end
 end
