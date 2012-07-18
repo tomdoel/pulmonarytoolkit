2e5d32ea842a9b4a896dc2e3d8d9347ad9e3814e
pulmonarytoolkit <pulmonarytoolkit@gmail.com>
2012-07-18 23:11:48 +0000

    install.m  - compilation is now output to the bin directory.  - Added compilation for mba_surface_interpolation

--- install.m
+++ install.m
@@ -14,8 +14,17 @@ disp('Compiling mex files');
 file_list = dir(fullfile('mex', '*.cpp'));
 for index = 1 : numel(file_list)
     filename = file_list(index).name;
-    mex(fullfile('mex', filename), '-outdir', 'mex');
+    mex_result = mex(fullfile('mex', filename), '-outdir', 'bin');
 end
+
+mex_result = mex('-outdir', 'bin', '-IExternal', ['-I' fullfile('External', 'mba', 'include')], ...
+    fullfile('External', 'gerardus', 'matlab', 'PointsToolbox', 'mba_surface_interpolation.cpp'), ...
+    fullfile('External', 'mba', 'src', 'MBA.cpp'), fullfile('External', 'mba', 'src', 'UCBsplines.cpp'), ...
+    fullfile('External', 'mba', 'src', 'UCBsplineSurface.cpp'), fullfile('External', 'mba', 'src', 'MBAdata.cpp'));
+
+
+
+
 
 disp('***********************************************************************');
 disp(' Install complete. If the mex compilation produced errors, you may need to adjust your compiler configuration. For more information run ''doc mex''.');
