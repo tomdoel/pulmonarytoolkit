a4cb0aacfbb97a172bb60c529665b40ff52dde52
pulmonarytoolkit@gmail.com <pulmonarytoolkit@gmail.com@032b9d54-5664-1725-390e-cfc7fbea0576>
2012-07-23 22:40:33 +0000

    TDAirwaysLabelledByLobe TDGetAirwaysLabelledByLobe  - Update functions to use the global coordinates returned by TDAirwaySkeleton as a TDTreeModel structure.

--- trunk/Library/TDGetAirwaysLabelledByLobe.m
+++ trunk/Library/TDGetAirwaysLabelledByLobe.m
@@ -1,4 +1,4 @@
-function [results_image, start_branches] = TDGetAirwaysLabelledByLobe(template, airway_results, skeleton_results, reporting)
+function [results_image, start_branches] = TDGetAirwaysLabelledByLobe(template, airway_results, skeleton_airway_tree, reporting)
     % TDGetAirwaysLabelledByLobe. Label segmented bronchi according to the
     % lobes they serve.
     % 
@@ -14,8 +14,8 @@ function [results_image, start_branches] = TDGetAirwaysLabelledByLobe(template, 
     %     airway_results : the root TDTreeSegment of a segmented airway tree
     %         structure produced by TDAirwayRegionGrowingWithExplosionControl
     %
-    %     skeleton_results : the root TDSkeletonSegment of a segmented airway
-    %         tree skeleton produced by TDSkeletonise
+    %     skeleton_airway_tree : the root TDTreeModel of a segmented airway
+    %         tree skeleton produced by TDAirwaySkeleton
     %
     %     reporting : A TDReporting object used for error and warning messages
     %
@@ -50,23 +50,22 @@ function [results_image, start_branches] = TDGetAirwaysLabelledByLobe(template, 
     %     Distributed under the GNU GPL v3 licence. Please see website for details.
     %
 
-    image_size = skeleton_results.image_size;
-    
-    start_segment = skeleton_results.airway_skeleton;
+    start_segment = skeleton_airway_tree;
 
     % Separate into left and right lungs
-    [left_lung_start, right_lung_start] = SeparateIntoLeftAndRightLungs(start_segment, image_size, reporting);
+    [left_lung_start, right_lung_start] = SeparateIntoLeftAndRightLungs(start_segment, template, reporting);
     
     % Separate left lung into upper and lower lobes
-    [left_upper_startindices, left_lower_startindices, uncertain_segments] = SeparateLeftLungIntoLobes(left_lung_start, image_size, reporting);
+    [left_upper_startindices, left_lower_startindices, uncertain_segments] = SeparateLeftLungIntoLobes(left_lung_start, template, reporting);
         
     % Separate right lung into upper and mid/lower lobes
-    [right_upper_startindices, right_midlower_startindices] = SeparateRightLungIntoUpperAndMidlowerLobes(right_lung_start, image_size, reporting);
+    [right_upper_startindices, right_midlower_startindices] = SeparateRightLungIntoUpperAndMidlowerLobes(right_lung_start, template, reporting);
     
     % Separate right mid/lower lobe into mid and lower lobes
-    [right_mid_startindices, right_lower_startindices] = SeparateRightLungIntoMidAndLowerLobes(right_midlower_startindices, image_size, reporting);
+    [right_mid_startindices, right_lower_startindices] = SeparateRightLungIntoMidAndLowerLobes(right_midlower_startindices, template, reporting);
 
     start_branches = [];
+    start_branches.Trachea = start_segment;
     start_branches.Left = left_lung_start;
     start_branches.Right = right_lung_start;
     start_branches.LeftUpper = left_upper_startindices;
@@ -75,85 +74,14 @@ function [results_image, start_branches] = TDGetAirwaysLabelledByLobe(template, 
     start_branches.RightMidLower = right_midlower_startindices;
     start_branches.RightMid = right_mid_startindices;
     start_branches.RightLower = right_lower_startindices;
+    start_branches.LeftUncertain = uncertain_segments;
     
-    
-    % Get voxels for lobes
-    left_lower_voxels = GetVoxelsForTheseBranches(left_lower_startindices);
-    left_upper_voxels = GetVoxelsForTheseBranches(left_upper_startindices);
-    right_lower_voxels = GetVoxelsForTheseBranches(right_lower_startindices);
-    right_mid_voxels = GetVoxelsForTheseBranches(right_mid_startindices);
-    right_upper_voxels = GetVoxelsForTheseBranches(right_upper_startindices);
-    
-    uncertain_voxels = GetVoxelsForTheseBranches(uncertain_segments);
-
-    left_lower_voxels_extended = GetVoxelsForTheseBranchesExtended(left_lower_startindices, image_size);
-    left_upper_voxels_extended = GetVoxelsForTheseBranchesExtended(left_upper_startindices, image_size);
-    right_lower_voxels_extended = GetVoxelsForTheseBranchesExtended(right_lower_startindices, image_size);
-    right_mid_voxels_extended = GetVoxelsForTheseBranchesExtended(right_mid_startindices, image_size);
-    right_upper_voxels_extended = GetVoxelsForTheseBranchesExtended(right_upper_startindices, image_size);
-    
-    uncertain_voxels_extended = GetVoxelsForTheseBranchesExtended(uncertain_segments, image_size);
-    
-    results_image = zeros(image_size, 'uint8');
-    
-    full_tree_voxels = start_segment.GetTree;
-    
-    results_image(full_tree_voxels) = 7;
-    
-    results_image(right_upper_voxels) = 1;
-    results_image(right_mid_voxels) = 2;
-    results_image(right_lower_voxels) = 4;
-    results_image(left_upper_voxels) = 5;
-    results_image(left_lower_voxels) = 6;
-    
-    results_image(uncertain_voxels) = 3;
-
-    % Label segments by skeleton
-    results_image(:) = 0;
-
-    segments_to_do = airway_results.AirwayTree;
-    
-    while ~isempty(segments_to_do)
-        segment = segments_to_do(end);
-        segments_to_do(end) = [];
-        voxel_indices_in_segment = segment.GetAllAirwayPoints;
-        voxel_indices_in_segment = template.GlobalToLocalIndices(voxel_indices_in_segment);
-        
-        lobe_indices = [];
-        if any(ismember(right_upper_voxels_extended, voxel_indices_in_segment))
-            lobe_indices = [lobe_indices, 1];
-        end
-        if any(ismember(right_mid_voxels_extended, voxel_indices_in_segment))
-            lobe_indices = [lobe_indices, 2];
-        end
-        if any(ismember(right_lower_voxels_extended, voxel_indices_in_segment))
-            lobe_indices = [lobe_indices, 4];
-        end
-        if any(ismember(left_upper_voxels_extended, voxel_indices_in_segment))
-            lobe_indices = [lobe_indices, 5];
-        end
-        if any(ismember(left_lower_voxels_extended, voxel_indices_in_segment))
-            lobe_indices = [lobe_indices, 6];
-        end
-        
-        if any(ismember(uncertain_voxels_extended, voxel_indices_in_segment))
-            lobe_indices = [lobe_indices, 3];
-        end
-        
-        if length(lobe_indices) == 1
-            results_image(voxel_indices_in_segment) = lobe_indices;
-        end
-        
-        segments_to_do = [segments_to_do segment.Children];
-    end
-    
+    results_image = TDColourBranchesByLobe(start_branches, airway_results.AirwayTree, template);
 end
 
 
-
-
 % Find the starting bronchi for the left and right lungs
-function [left_lung_start, right_lung_start] = SeparateIntoLeftAndRightLungs(start_segment, image_size, reporting)
+function [left_lung_start, right_lung_start] = SeparateIntoLeftAndRightLungs(start_segment, template, reporting)
     if length(start_segment.Children) > 3
        reporting.Error('TDGetAirwaysLabelledByLobe:TooManyBronchi', 'Main bronchus branches into more than 3 bronchi.'); 
     elseif length(start_segment.Children) < 2
@@ -163,11 +91,11 @@ function [left_lung_start, right_lung_start] = SeparateIntoLeftAndRightLungs(sta
     child_segment_1 = start_segment.Children(1);
     child_segment_2 = start_segment.Children(2);
 
-    tree_1 = child_segment_1.GetTree;
-    tree_2 = child_segment_2.GetTree;
+    tree_1 = CentrelinePointsToLocalIndices(child_segment_1.GetCentrelineTree, template);
+    tree_2 = CentrelinePointsToLocalIndices(child_segment_2.GetCentrelineTree, template);
     
-    centroid_1 = GetCentroid(tree_1, image_size);
-    centroid_2 = GetCentroid(tree_2, image_size);
+    centroid_1 = GetCentroid(tree_1, template);
+    centroid_2 = GetCentroid(tree_2, template);
     
     if centroid_1(2) < centroid_2(2)
         right_lung_start = child_segment_1;
@@ -178,13 +106,13 @@ function [left_lung_start, right_lung_start] = SeparateIntoLeftAndRightLungs(sta
     end
 end
 
-function [left_upper_startsegment, left_lower_startsegment, uncertain] = SeparateLeftLungIntoLobes(left_lung_start, image_size, reporting)
+function [left_upper_startsegment, left_lower_startsegment, uncertain] = SeparateLeftLungIntoLobes(left_lung_start, template, reporting)
     if length(left_lung_start.Children) > 2
         reporting.ShowWarning('TDGetAirwaysLabelledByLobe:TooManyBronchi', 'More than 2 bronchial branches were found separating the left upper and lower lobes.', []);
         left_lower_startsegment = [];
         left_upper_startsegment = [];
     elseif length(left_lung_start.Children) < 1
-        calback.ShowWarning('TDGetAirwaysLabelledByLobe:TooManyBronchi', 'ERROR: No bronchial branches were found separating the left upper and lower lobes.', []);
+        calback.ShowWarning('TDGetAirwaysLabelledByLobe:NoBronchi', 'ERROR: No bronchial branches were found separating the left upper and lower lobes.', []);
         left_lower_startsegment = [];
         left_upper_startsegment = [];
     else
@@ -195,16 +123,16 @@ function [left_upper_startsegment, left_lower_startsegment, uncertain] = Separat
         first_children = first_bifurcation.Children;
         first_grandchildren = [first_children(1).Children, first_children(2).Children];
         
-        ordered_grandchildren = OrderSegmentsByCentroidDistanceFromDiagonalPlane(first_grandchildren, image_size);
+        ordered_grandchildren = OrderSegmentsByCentroidDistanceFromDiagonalPlane(first_grandchildren, template);
         
         lower_lobe_reference = ordered_grandchildren(1);
         upper_lobe_reference = ordered_grandchildren(end);
         uncertain_segments = ordered_grandchildren(2 : end-1);
         
-        upper_lobe_dpcentroid = GetDPCentroid(upper_lobe_reference, image_size);
-        lower_lobe_dpcentroid = GetDPCentroid(lower_lobe_reference, image_size);
+        upper_lobe_dpcentroid = GetDPCentroid(upper_lobe_reference, template);
+        lower_lobe_dpcentroid = GetDPCentroid(lower_lobe_reference, template);
 
-        lower_lobe_3centroid = GetCentroid(lower_lobe_reference.GetTree, image_size);
+        lower_lobe_3centroid = GetCentroid(CentrelinePointsToLocalIndices(lower_lobe_reference.GetCentrelineTree, template), template);
 
         if abs(upper_lobe_dpcentroid - lower_lobe_dpcentroid) < 20
             reporting.Error('TDGetAirwaysLabelledByLobe:NotEnoughCentroidSeparation', 'Unable to determine airway branching structure because there is not enough separation between the centroids of the skeletonised branches');
@@ -214,8 +142,8 @@ function [left_upper_startsegment, left_lower_startsegment, uncertain] = Separat
         left_lower_startsegment = lower_lobe_reference;
         
         for i = 1 : length(uncertain_segments)
-            uncertain_dpcentroid = GetDPCentroid(uncertain_segments(i), image_size);
-            uncertain_3centroid = GetCentroid(uncertain_segments(i).GetTree, image_size);
+            uncertain_dpcentroid = GetDPCentroid(uncertain_segments(i), template);
+            uncertain_3centroid = GetCentroid(CentrelinePointsToLocalIndices(uncertain_segments(i).GetCentrelineTree, template), template);
             distance_to_upper_lobe = abs(uncertain_dpcentroid - upper_lobe_dpcentroid);
             distance_to_lower_lobe = abs(uncertain_dpcentroid - lower_lobe_dpcentroid);
             
@@ -255,7 +183,7 @@ function [left_upper_startsegment, left_lower_startsegment, uncertain] = Separat
     end
 end
 
-function [right_upper_startindices, right_midlower_startindices] = SeparateRightLungIntoUpperAndMidlowerLobes(right_lung_startindex, image_size, reporting)
+function [right_upper_startindices, right_midlower_startindices] = SeparateRightLungIntoUpperAndMidlowerLobes(right_lung_startindex, template, reporting)
     if numel(right_lung_startindex) > 1
         reporting.Error('TDGetAirwaysLabelledByLobe:TooManyStartingIndices', 'Too many start indices for the right upper lobe');
     end
@@ -272,8 +200,8 @@ function [right_upper_startindices, right_midlower_startindices] = SeparateRight
         child_index_1 = right_lung_startindex.Children(1);
         child_index_2 = right_lung_startindex.Children(2);
         
-        centroid_1 = GetDPCentroid(child_index_1, image_size);
-        centroid_2 = GetDPCentroid(child_index_2, image_size);
+        centroid_1 = GetDPCentroid(child_index_1, template);
+        centroid_2 = GetDPCentroid(child_index_2, template);
         
         if abs(centroid_1 - centroid_2) < 20
             reporting.Error('TDGetAirwaysLabelledByLobe:NotEnoughCentroidSeparation', 'Unable to determine airway branching structure because there is not enough separation between the centroids of the skeletonised branches');
@@ -289,7 +217,7 @@ function [right_upper_startindices, right_midlower_startindices] = SeparateRight
     end
 end
 
-function [right_mid_startindices, right_lower_startindices] = SeparateRightLungIntoMidAndLowerLobes(right_midlower_startindex, image_size, reporting)
+function [right_mid_startindices, right_lower_startindices] = SeparateRightLungIntoMidAndLowerLobes(right_midlower_startindex, template, reporting)
     if isempty(right_midlower_startindex)
         reporting.Error('TDGetAirwaysLabelledByLobe:NoStartBranch', 'No start branch was specified.');
     end
@@ -306,12 +234,12 @@ function [right_mid_startindices, right_lower_startindices] = SeparateRightLungI
         right_mid_startindices = [];
         right_lower_startindices = [];
     else
-        sorted_child_indices_A = OrderByCentroidI(right_midlower_startindex, image_size);
+        sorted_child_indices_A = OrderByCentroidI(right_midlower_startindex, template);
         child_index_A1 = sorted_child_indices_A(1);
         child_index_A2 = sorted_child_indices_A(2);
 
-        centroid_A1 = GetICentroid(child_index_A1, image_size);
-        centroid_A2 = GetICentroid(child_index_A2, image_size);
+        centroid_A1 = GetICentroid(child_index_A1, template);
+        centroid_A2 = GetICentroid(child_index_A2, template);
         if abs(centroid_A1 - centroid_A2) < 10
             reporting.ShowWarning('TDGetAirwaysLabelledByLobe:NotEnoughCentroidSeparation', 'Not enough centroid separation.', []);
         end
@@ -327,12 +255,12 @@ function [right_mid_startindices, right_lower_startindices] = SeparateRightLungI
 
         child_indices_B = right_mid_startindices.Children;
         if ~isempty(child_indices_B)
-            sorted_child_indices_B = OrderByCentroidI(child_index_A1, image_size);
+            sorted_child_indices_B = OrderByCentroidI(child_index_A1, template);
             child_index_B1 = sorted_child_indices_B(1);
             child_index_B2 = sorted_child_indices_B(2);
             
-            centroid_B1 = GetICentroid(child_index_B1, image_size);
-            centroid_B2 = GetICentroid(child_index_B2, image_size);
+            centroid_B1 = GetICentroid(child_index_B1, template);
+            centroid_B2 = GetICentroid(child_index_B2, template);
            
             distance_1 = centroid_A2 - centroid_B2;
             distance_2 = centroid_B2 - centroid_B1;
@@ -353,38 +281,6 @@ function [right_mid_startindices, right_lower_startindices] = SeparateRightLungI
 end
 
 
-function voxels = GetVoxelsForTheseBranches(start_indices)
-    voxels = [];
-    for index = 1 : numel(start_indices)
-        voxels = cat(2, voxels, start_indices(index).GetTree);
-    end
-end
-
-    
-function voxels = GetVoxelsForTheseBranchesExtended(start_indices, image_size)
-    voxels = [];
-    if isempty(start_indices)
-        return;
-    end
-    
-    for index = 1 : numel(start_indices)
-        voxels = cat(2, voxels, start_indices(index).GetTree);
-        parent = start_indices(index).Parent;
-        while ~isempty(parent)
-            voxels = cat(2, voxels, parent.Points);
-            parent = parent.Parent;
-        end
-    end
-
-    % Add nearest neighbours to the list of voxels, otherwise it is possible for
-    % a diagnoally-connected skeleton segment to pass through a
-    % diagnoally-connected airway segment
-    [~, linear_offsets27] = TDImageCoordinateUtilities.GetLinearOffsets(image_size);
-    voxels = repmat(int32(voxels), 27, 1) + repmat(int32(linear_offsets27'), 1, length(voxels));    
-    voxels = unique(voxels(:));
-end
-
-
 
 function sorted_child_indices = OrderByCentroidI(start, image_size)
     child_indices = start.Children;
@@ -397,33 +293,38 @@ function sorted_child_indices = OrderByCentroidI(start, image_size)
     sorted_child_indices = child_indices(sorted_indices);
 end
 
-function sorted_segments = OrderSegmentsByCentroidDistanceFromDiagonalPlane(segments_to_order, image_size)
+function sorted_segments = OrderSegmentsByCentroidDistanceFromDiagonalPlane(segments_to_order, template)
     centroids_dp = [];
     for i = 1 : length(segments_to_order)
-        centroids_dp(end + 1) = GetDPCentroid(segments_to_order(i), image_size);
+        centroids_dp(end + 1) = GetDPCentroid(segments_to_order(i), template);
     end
     
     [~, sorted_indices] = sort(centroids_dp);
     sorted_segments = segments_to_order(sorted_indices);
 end
 
-function centroid_i = GetICentroid(start, image_size)
-    tree = start.GetTree;
-    centroid = GetCentroid(tree, image_size);
+function centroid_i = GetICentroid(start, template)
+    tree = CentrelinePointsToLocalIndices(start.GetCentrelineTree, template);
+    centroid = GetCentroid(tree, template);
     centroid_i = centroid(1);
 end
 
-function centroid_dp = GetDPCentroid(start_segment, image_size)
-    tree = start_segment.GetTree;
-    centroid = GetCentroid(tree, image_size);
+function centroid_dp = GetDPCentroid(start_segment, template)
+    tree = CentrelinePointsToLocalIndices(start_segment.GetCentrelineTree, template);
+    centroid = GetCentroid(tree, template);
     centroid_dp = - centroid(3) - centroid(1);
 end
 
-function centroid = GetCentroid(indices, image_size)
-    [i, j, k] = ind2sub(image_size, indices);
+function centroid = GetCentroid(indices, template)
+    [i, j, k] = ind2sub(template.ImageSize, indices);
     centroid = zeros(1, 3);
     centroid(1) = mean(i);
     centroid(2) = mean(j);
     centroid(3) = mean(k);    
+end
+
+function centreline_indices_local = CentrelinePointsToLocalIndices(centreline_points, template_image)
+    centreline_indices_global = [centreline_points.GlobalIndex];
+    centreline_indices_local = template_image.GlobalToLocalIndices(centreline_indices_global);
 end
 
