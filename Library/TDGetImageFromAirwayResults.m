cda1cc94739f23c3526d537ff3e9b540424b4e41
pulmonarytoolkit <pulmonarytoolkit@gmail.com>
2012-07-23 22:27:53 +0000

    TDAirways.m  - generation of the airway image is now outsourced to TDGetImageFromAirwayResults.
    TDGetImageFromAirwayResults.m
     - new function for constructing a segmented airway image from the airway tree results.

---
+++ Library/TDGetImageFromAirwayResults.m
@@ -0,0 +1,34 @@
+function airway_image = TDGetImageFromAirwayResults(airway_results, template_image, reporting)
+    % TDGetImageFromAirwayResults. Creates an image of the segmented airways
+    %
+    %     This function takes the airway results structure from the TDAirways
+    %     plugin and creates an image of the segmented airways.
+    %
+    %
+    %     Licence
+    %     -------
+    %     Part of the TD Pulmonary Toolkit. http://code.google.com/p/pulmonarytoolkit
+    %     Author: Tom Doel, 2012.  www.tomdoel.com
+    %     Distributed under the GNU GPL v3 licence. Please see website for details.
+    %
+    
+    image_size = template_image.ImageSize;
+    
+    airway_image_raw = zeros(image_size, 'uint8');
+    
+    airway_tree = airway_results.AirwayTree;
+    segments_to_do = airway_tree;
+    while ~isempty(segments_to_do)
+        segment = segments_to_do(end);
+        segments_to_do(end) = [];
+        voxels = segment.GetAllAirwayPoints;
+        voxels = template_image.GlobalToLocalIndices(voxels);
+        airway_image_raw(voxels) = 1;
+        segments_to_do = [segments_to_do, segment.Children];
+    end
+    
+    airway_image = template_image.BlankCopy;
+    airway_image.ChangeRawImage(airway_image_raw);
+    airway_image.ImageType = TDImageType.Colormap;
+end
+
