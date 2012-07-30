4b629d511de11a0c5a92b4501f3352db8bebda42
pulmonarytoolkit@gmail.com <pulmonarytoolkit@gmail.com@032b9d54-5664-1725-390e-cfc7fbea0576>
2012-07-30 11:54:14 +0000

    TDProgressPanel  - Substantial improvements to make the progress reporting panel faster and more responsive

--- trunk/Components/TDProgressPanel.m
+++ trunk/Components/TDProgressPanel.m
@@ -15,26 +15,36 @@ classdef TDProgressPanel < TDProgressInterface
     %    
     
     properties (Access = private)
-        PanelHandle
-        Disabled = false
         
-        ProgressBarHandle
-        Text
-        Title
-        Cancel
-        Quit
-
+        % Handles to the GUI components
         Parent
+        PanelHandle
+        UiControlTitle
+        UiControlText
+        UiControlCancel
+        UiControlQuit
+        ProgressBarHandle
+
         IncrementThreshold = 5
+
+        % Flags
+        Hold = false;
+        ShowProgressBar = false
+        UserClickedCancel = false
+
+        % The current values
         DialogText
         DialogTitle = ''
         ProgressValue = 0
+        PanelVisibility = false;
         
-        Hold = false;
-        ShowProgressBar = false
-
-        UserClickedCancel = false
         
+        % The previous values
+        CurrentlyDisplayedDialogTitle
+        CurrentlyDisplayedDialogText
+        CurrentlyDisplayedProgressValue
+        CurrentlyDisplayedVisibility = false
+        ProgressBarCurrentlyVisible = false;
     end
     
     properties (GetAccess = private, Constant)
@@ -47,12 +57,16 @@ classdef TDProgressPanel < TDProgressInterface
         
         
         function obj = TDProgressPanel(parent)
+            obj.CurrentlyDisplayedDialogTitle = '';
+            obj.CurrentlyDisplayedDialogText = '';
+            obj.CurrentlyDisplayedProgressValue = [];
+            
             obj.Parent = parent;
             
             set(parent, 'Units', 'Pixels');
             
             % Create the panel
-            panel_background_colour = [0.0 0.129 0.278];
+            panel_background_colour = TDSoftwareInfo.BackgroundColour;
             progress_position = obj.GetPanelPosition;
             obj.PanelHandle = uipanel('Parent', parent, 'Title', '', 'BorderType', 'etchedin', 'ForegroundColor', 'white', ...
                 'BackgroundColor', panel_background_colour, 'Units', 'pixels', 'Position', progress_position, 'Visible', 'off' ...
@@ -60,22 +74,23 @@ classdef TDProgressPanel < TDProgressInterface
             
             title_position = [20, 165, 460, 55];
             text_position = [20, 115, 460, 45];
-            cancel_position = [70, 20, 150, 40];
-            quit_position = [280, 20, 150, 40];
+            cancel_position = [175, 20, 150, 40];
+            quit_position = [350, 20, 80, 40];
             progress_bar_position = [50, 90, 400, 18];
             
             
-            obj.Title = uicontrol('parent', obj.PanelHandle, 'style', 'text', 'units', 'pixel', 'Position', title_position, ...
+            obj.UiControlTitle = uicontrol('parent', obj.PanelHandle, 'style', 'text', 'units', 'pixel', 'Position', title_position, ...
                 'string', 'Please wait', 'FontUnits', 'pixels', 'FontSize', 26, 'FontWeight', 'bold', 'Fore', 'white', 'Back', [0, 0.129, 0.278], 'Visible', 'off');
-            obj.Text = uicontrol('parent', obj.PanelHandle, 'style', 'text', 'units', 'pixel', 'Position', text_position, ...
+            obj.UiControlText = uicontrol('parent', obj.PanelHandle, 'style', 'text', 'units', 'pixel', 'Position', text_position, ...
                 'string', 'Please wait', 'FontUnits', 'pixels', 'FontSize', 16, 'Fore', 'white', 'Back', [0, 0.129, 0.278], 'Visible', 'off');
-            obj.Cancel = uicontrol('parent', obj.PanelHandle, 'string', 'Cancel', ...
+            obj.UiControlCancel = uicontrol('parent', obj.PanelHandle, 'string', 'Cancel', ...
                 'Position', cancel_position, 'FontUnits', 'pixels', 'Callback', @obj.CancelButton, 'Visible', 'off');
-            obj.Quit = uicontrol('parent', obj.PanelHandle, 'string', 'Force Quit', ...
+            obj.UiControlQuit = uicontrol('parent', obj.PanelHandle, 'string', 'Force Quit', ...
                 'Position', quit_position, 'FontUnits', 'pixels', 'Callback', @obj.QuitButton, 'Visible', 'off');
             
             [obj.ProgressBarHandle, ~] = javacomponent('javax.swing.JProgressBar', ...
                 progress_bar_position, obj.PanelHandle);
+            obj.ProgressBarHandle.setBackground(java.awt.Color(panel_background_colour(1), panel_background_colour(2), panel_background_colour(3)));
             set(obj.ProgressBarHandle, 'visible', 0);
         end
         
@@ -87,24 +102,22 @@ classdef TDProgressPanel < TDProgressInterface
             if nargin < 2
                 text = 'Please wait';
             end
-            obj.Hide;
             obj.DialogTitle = TDTextUtilities.RemoveHtml(text);
             obj.DialogText = '';
             obj.ProgressValue = 0;
             obj.Hold = true;
-            obj.Update;
-            obj.ShowPanel;
             obj.UserClickedCancel = false;
-            drawnow;
+            obj.PanelVisibility = true;
+            obj.ShowProgressBar = false;
+            obj.Update;
         end
         
         function Hide(obj)
             obj.DialogTitle = 'Please wait';
-            obj.ShowProgressBar = false;
-
-            obj.HidePanel;
             obj.Hold = false;
-            drawnow;
+            obj.PanelVisibility = false;
+            obj.ShowProgressBar = false;
+            obj.Update;
         end
         
         % Call to complete a progress operaton, which will also hide the dialog
@@ -113,11 +126,10 @@ classdef TDProgressPanel < TDProgressInterface
             obj.ShowProgressBar = false;
             obj.ProgressValue = 100;
             if ~obj.Hold
-                obj.Hide;
+                obj.PanelVisibility = false;
             else
-                obj.Update;
             end
-            drawnow;
+            obj.Update;
         end
         
         function SetProgressText(obj, text)
@@ -125,26 +137,23 @@ classdef TDProgressPanel < TDProgressInterface
                text = 'Please wait'; 
             end            
             obj.DialogText = TDTextUtilities.RemoveHtml(text);
+            obj.PanelVisibility = true;
             obj.Update;
-            obj.ShowPanel;            
-            drawnow;
         end
         
         function SetProgressValue(obj, progress_value)
-            obj.ProgressValue = progress_value;
             obj.ShowProgressBar = true;
+            obj.ProgressValue = progress_value;
+            obj.PanelVisibility = true;
             obj.Update;
-            obj.ShowPanel;
-            drawnow;
         end
         
         function SetProgressAndMessage(obj, progress_value, text)
             obj.ShowProgressBar = true;
             obj.DialogText = TDTextUtilities.RemoveHtml(text);
             obj.ProgressValue = progress_value;
+            obj.PanelVisibility = true;
             obj.Update;
-            obj.ShowPanel;            
-            drawnow;
         end
         
         function cancelled = CancelClicked(obj)
@@ -154,44 +163,96 @@ classdef TDProgressPanel < TDProgressInterface
     end
     
     methods (Access = private)
-        function Update(obj)
-            set(obj.Title, 'String', obj.DialogTitle);
-            set(obj.Text, 'String', obj.DialogText);
-            obj.ProgressBarHandle.setValue(obj.ProgressValue);
-        end
-        
-        function ShowPanel(obj)
-            if obj.Disabled
-                return;
-            end            
-            set(obj.PanelHandle, 'Visible', 'on');
-            set(obj.Text, 'Visible', 'on');
-            set(obj.Title, 'Visible', 'on');
-            set(obj.Quit, 'Visible', 'on');
-            set(obj.Cancel, 'Visible', 'on');
-            set(obj.ProgressBarHandle, 'visible', 1);
-        end
-        
-        function HidePanel(obj)
-            if obj.Disabled
-                return;
+        function changed = Update(obj)
+            changed = false;
+            changed = obj.UpdateDialogTitle || changed;
+            changed = obj.UpdateDialogText || changed;
+            changed = obj.UpdateProgressValue || changed;
+            changed = obj.UpdateVisibility || changed;
+            changed = obj.UpdateProgressBarVisibility || changed;
+            if changed
+                drawnow;
+                changed = false;
             end
-            
-            set(obj.Text, 'Visible', 'off');
-            set(obj.Title, 'Visible', 'off');
-            set(obj.Quit, 'Visible', 'off');
-            set(obj.Cancel, 'Visible', 'off');
-            set(obj.ProgressBarHandle, 'visible', 0);
-            set(obj.PanelHandle, 'Visible', 'off');
+        end
+        
+        function changed = UpdateDialogTitle(obj)
+            changed = false;
+            if ~strcmp(obj.DialogTitle, obj.CurrentlyDisplayedDialogTitle)
+                set(obj.UiControlTitle, 'String', obj.DialogTitle);
+                obj.CurrentlyDisplayedDialogTitle = obj.DialogTitle;
+                changed = true;
+            end
+        end
+        
+        function changed = UpdateDialogText(obj)
+            changed = false;
+            if ~strcmp(obj.DialogText, obj.CurrentlyDisplayedDialogText)
+                set(obj.UiControlText, 'String', obj.DialogText);
+                obj.CurrentlyDisplayedDialogText = obj.DialogText;
+                changed = true;
+            end            
+        end
+        
+        function changed = UpdateProgressValue(obj)
+            changed = false;
+            if ~isequal(obj.ProgressValue, obj.CurrentlyDisplayedProgressValue)
+                obj.ProgressBarHandle.setValue(obj.ProgressValue);
+                obj.CurrentlyDisplayedProgressValue = obj.ProgressValue;
+                changed = true;
+            end            
+        end
+        
+        function changed = UpdateProgressBarVisibility(obj)
+            changed = false;
+            if ~obj.ProgressBarCurrentlyVisible && obj.ShowProgressBar && obj.PanelVisibility 
+                set(obj.ProgressBarHandle, 'visible', 1);
+                obj.ProgressBarCurrentlyVisible = true;
+                changed = true;
+            elseif obj.ProgressBarCurrentlyVisible && (~obj.ShowProgressBar || ~obj.PanelVisibility)
+                set(obj.ProgressBarHandle, 'visible', 0);
+                obj.ProgressBarCurrentlyVisible = false;
+                changed = true;
+            end
+        end
+        
+        function changed = UpdateVisibility(obj)
+            changed = false;
+            if obj.PanelVisibility && ~obj.CurrentlyDisplayedVisibility
+                set(obj.PanelHandle, 'Visible', 'on');
+                set(obj.UiControlText, 'Visible', 'on');
+                set(obj.UiControlTitle, 'Visible', 'on');
+                if TDSoftwareInfo.DebugMode
+                    set(obj.UiControlQuit, 'Visible', 'on');
+                end
+                set(obj.UiControlCancel, 'Visible', 'on');
+                obj.CurrentlyDisplayedVisibility = true;
+                changed = true;
+            elseif ~obj.PanelVisibility && obj.CurrentlyDisplayedVisibility
+                set(obj.UiControlText, 'Visible', 'off');
+                set(obj.UiControlTitle, 'Visible', 'off');
+                set(obj.UiControlQuit, 'Visible', 'off');
+                set(obj.UiControlCancel, 'Visible', 'off');
+                set(obj.ProgressBarHandle, 'visible', 0);
+                set(obj.PanelHandle, 'Visible', 'off');
+                obj.CurrentlyDisplayedVisibility = false;
+                obj.ProgressBarCurrentlyVisible = false;
+                changed = true;
+            end
         end
         
         function CancelButton(obj, ~, ~)
             obj.UserClickedCancel = true;
+            obj.PanelVisibility = false;
+            obj.Hold = false;
+            obj.Update;
         end
         
         function QuitButton(obj, ~, ~)
-            obj.Hide;
-            throw(MException('TDCustomProgressDialog:UserForceQuit', 'User forced plugin to terminate'));
+            obj.PanelVisibility = false;
+            obj.Hold = false;
+            obj.UpdateVisibility;
+            throw(MException('TDProgressPanel:UserForceQuit', 'User forced plugin to quit'));
         end
 
         function progress_position = GetPanelPosition(obj)
