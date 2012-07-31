fecc0124f47729c28b827b86a18b5acfbf8ab2db
pulmonarytoolkit <pulmonarytoolkit@gmail.com>
2012-07-31 20:20:46 +0000

    TDVisualiseAirwayGrowingTree.m  - Change order of visualisation of tree branches

--- Library/TDVisualiseAirwayGrowingTree.m
+++ Library/TDVisualiseAirwayGrowingTree.m
@@ -39,42 +39,43 @@ function TDVisualiseAirwayGrowingTree(airway_tree, reporting)
     view(-37.5, 30);
     cl = camlight('headlight');
 
-    segments_to_do = airway_tree;
+    next_generation = airway_tree;
     segment_count = 0;
     total_segments = airway_tree.CountBranches;
     
-    while ~isempty(segments_to_do)
-        if reporting.HasBeenCancelled
-            return
-        end
-        segment_count = segment_count + 1;
+    while ~isempty(next_generation)
+        current_generation = next_generation;
+        next_generation = [];
+        
         reporting.UpdateProgressValue(round(100*segment_count/total_segments));
-        segment = segments_to_do(end);
-        segments_to_do(end) = [];
-        segments_to_do = [segments_to_do segment.Children];
-        
-        start_point = segment.StartCoords;
-        end_point = segment.EndCoords;
-        generation = segment.GenerationNumber;
-        
-        colour_value = mod(generation - 1, 5) + 1;
-        colour = cm(colour_value, :);
 
-        if segment.IsGenerated
-            thickness = 1;
-        else
-             colour = [1 1 0];
-            thickness = 2;
+        for segment = current_generation
+
+            segment_count = segment_count + 1;
+            next_generation = [next_generation segment.Children];
+        
+            start_point = segment.StartCoords;
+            end_point = segment.EndCoords;
+            generation = segment.GenerationNumber;
+            
+            colour_value = mod(generation - 1, 5) + 1;
+            colour = cm(colour_value, :);
+            
+            if segment.IsGenerated
+                thickness = 1;
+            else
+                colour = [1 1 0];
+                thickness = 2;
+            end
+
+            pi = [start_point(1); end_point(1)];
+            pj = [start_point(2); end_point(2)];
+            pk = [start_point(3); end_point(3)];
+            pk = - pk;
+            
+            
+            line('XData', pj, 'YData', pk, 'ZData', pi, 'Color', colour, 'LineWidth', thickness);
         end
-        
-        
-        pi = [start_point(1); end_point(1)];
-        pj = [start_point(2); end_point(2)];
-        pk = [start_point(3); end_point(3)];
-        pk = - pk;
-        
-        
-        line('XData', pj, 'YData', pk, 'ZData', pi, 'Color', colour, 'LineWidth', thickness);
     end
 end
 
