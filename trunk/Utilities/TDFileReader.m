4a97d7bcf1c458b5618343c10ab53863433b32b5
pulmonarytoolkit@gmail.com <pulmonarytoolkit@gmail.com@032b9d54-5664-1725-390e-cfc7fbea0576>
2012-08-25 23:06:56 +0000

    Added TDFileReader.m

---
+++ trunk/Utilities/TDFileReader.m
@@ -0,0 +1,52 @@
+classdef TDFileReader < handle
+    % TDFileReader. A helper class to assist with parsing a text file
+    %
+    %
+    %     Licence
+    %     -------
+    %     Part of the TD Pulmonary Toolkit. http://code.google.com/p/pulmonarytoolkit
+    %     Author: Tom Doel, 2012.  www.tomdoel.com
+    %     Distributed under the GNU GPL v3 licence. Please see website for details.
+    %
+
+    properties
+        FileText
+        FileId
+    end
+    
+    methods
+        function obj = TDFileReader(file_path, file_name, reporting)
+            full_file_name = fullfile(file_path, file_name);
+            file_id = fopen(full_file_name, 'rt');
+            if (file_id == -1)
+                reporting.Error('TDFileReader:CannotOpenFile', ['Unable to open file ' full_file_name]);
+            else
+                obj.FileId = file_id;
+            end
+        end
+        
+        function Close(obj)
+            if ~isempty(obj.FileId)
+                fclose(obj.FileId);
+                obj.FileId = [];
+            end
+        end
+        
+        function delete(obj)
+            obj.Close;
+        end
+        
+        function next_line = NextLine(obj)
+            next_line = [];
+            while isempty(next_line)
+                next_line = fgetl(obj.FileId);
+                next_line = strtrim(next_line);
+            end
+        end
+        
+        function data = ReadData(obj, format_string, num_points)
+            data = textscan(obj.FileId, format_string, num_points);
+        end
+    end
+end
+
