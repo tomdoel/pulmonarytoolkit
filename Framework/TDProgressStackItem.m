3666975e9123cbb4eb0c4cad5ab3f5ea74db3ad7
pulmonarytoolkit <pulmonarytoolkit@gmail.com>
2012-09-25 13:53:03 +0000

    TDProgressStackItem.m  - Part of a new mechanism for nesting progress reporting

---
+++ Framework/TDProgressStackItem.m
@@ -0,0 +1,37 @@
+classdef TDProgressStackItem < handle
+    % TDProgressStackItem. Part of the internal framework of the Pulmonary Toolkit.
+    %
+    %     You should not use this class within your own code. It is intended to
+    %     be used internally within the framework of the Pulmonary Toolkit.
+    %
+    %     TDProgressStackItem is part of the mechanism used to nest progress
+    %     reporting, so that for example, if an operation is performed 4 times,
+    %     the progress bar will not go from 0 to 100% 3 times, but instead go
+    %     from 0 to 25% for the first operation, etc.
+    %
+    %     Licence
+    %     -------
+    %     Part of the TD Pulmonary Toolkit. http://code.google.com/p/pulmonarytoolkit
+    %     Author: Tom Doel, 2012.  www.tomdoel.com
+    %     Distributed under the GNU GPL v3 licence. Please see website for details.
+    %
+
+    properties
+        ProgressText
+        MinPosition
+        MaxPosition
+        LastProgressValue
+    end
+    
+    methods
+        function obj = TDProgressStackItem(text, min_pos, max_pos)
+            if nargin > 0
+                obj.ProgressText = text;
+                obj.MinPosition = min_pos;
+                obj.MaxPosition = max_pos;
+                obj.LastProgressValue = 0;
+            end
+        end
+    end
+end
+
