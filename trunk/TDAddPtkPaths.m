376133e4cbe1e1d8adba8595325e8b5a7220d0c0
pulmonarytoolkit@gmail.com <pulmonarytoolkit@gmail.com@032b9d54-5664-1725-390e-cfc7fbea0576>
2012-10-05 21:59:17 +0000

    Moved files into Library

--- trunk/TDAddPtkPaths.m
+++ trunk/TDAddPtkPaths.m
@@ -22,15 +22,15 @@ function TDAddPtkPaths(varargin)
         path_folders{end + 1} = 'Gui';
         path_folders{end + 1} = 'GuiPlugins';
         path_folders{end + 1} = 'Plugins';
-        path_folders{end + 1} = 'Utilities';
         path_folders{end + 1} = 'Library';
         path_folders{end + 1} = fullfile('Library', 'Airways');
         path_folders{end + 1} = fullfile('Library', 'File');
         path_folders{end + 1} = fullfile('Library', 'GuiComponents');
+        path_folders{end + 1} = fullfile('Library', 'Interfaces');
         path_folders{end + 1} = fullfile('Library', 'Registration');
         path_folders{end + 1} = fullfile('Library', 'Types');
+        path_folders{end + 1} = fullfile('Library', 'Utilities');
         path_folders{end + 1} = fullfile('Library', 'Visualisation');
-        path_folders{end + 1} = 'Interfaces';
         path_folders{end + 1} = 'Framework';
         path_folders{end + 1} = fullfile('External', 'gerardus', 'matlab', 'PointsToolbox');
         
