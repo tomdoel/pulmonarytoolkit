e075d8f922699f0bf2a14245deeff2de2dbe06d6
pulmonarytoolkit <pulmonarytoolkit@gmail.com>
2012-12-03 01:27:07 +0000

    TDComputeDensityAverage.m  - Refactored density computation so that it now excludes the airway lumen and surrounding voxels.  - Removal of voxels near the surface and the airways is now resolution-independent  - Voxels with fewer than half of their neighbours in their mask are removed  - The mask is also returned

---
+++ Library/Analysis/TDComputeDensityAverage.m
@@ -0,0 +1,87 @@
+function [density_average, density_average_mask] = TDComputeDensityAverage(lung_roi, mask, airways, reporting)
+    % TDComputeDensityAverage. Computes the density of parenchymal tissue by averaging over neighbouring voxels
+    %
+    % Given a DICOM lung image, returns the density of voxels in the lung
+    % parenchyma, averaged over a 5x5x5 voxel neighbourgood. Voxels close to the
+    % image boundaries and in the airways are excluded.
+    %
+    % Syntax:
+    %     top_of_trachea = TDFindTopOfTrachea(lung_image, reporting)
+    %     [top_of_trachea, trachea_voxels] = TDFindTopOfTrachea(lung_image, reporting)
+    %
+    % Inputs:
+    %     lung_roi - a TDImage containing the raw image values
+    %
+    %     mask - a TDImage mask containing the voxels inside the lung
+    %
+    %     reporting (optional) - an object implementing the TDReporting
+    %         interface for reporting progress and warnings
+    %
+    % Outputs:
+    %     density_average - a TDImage containing average density values in g/mL
+    %
+    %     density_average_mask - a TDImage containing a mask of voxels whose
+    %         densities were calculated
+    %
+    %
+    %     Licence
+    %     -------
+    %     Part of the TD Pulmonary Toolkit. http://code.google.com/p/pulmonarytoolkit
+    %     Author: Tom Doel, 2012.  www.tomdoel.com
+    %     Distributed under the GNU GPL v3 licence. Please see website for details.
+    %
+    
+    
+    if ~isa(lung_roi, 'TDImage') || ~isa(mask, 'TDImage') || ~isa(airways, 'TDImage')
+        reporting.Error('TDComputeDensityAverage:InvalidInput', 'Requires a TDImage for all inputs');
+    end
+    
+    if nargin < 4
+        reporting = TDReportingDefault;
+    end
+    
+    % The neighbourhood used to compute the density is a structural element
+    density_average_el = ones(5,5,5);
+    size_el = numel(density_average_el);
+    
+    % Create a mask comprising of the lung exterior and the segmented airways
+    reporting.ShowProgress('Creating a parenchyma mask excluding borders and airways');
+    density_average_mask = mask.BlankCopy;
+    density_average_mask.ChangeRawImage((mask.RawImage == 0) | (airways.RawImage == 1));
+    
+    % Expand mask to include neighbouring voxels
+    density_average_mask.BinaryMorph(@imdilate, 3);
+    
+    % Invert mask so it now represents the lung parenchyma
+    density_average_mask.ChangeRawImage(~density_average_mask.RawImage);
+    
+    % Remove the mask from the lung image
+    lung_roi = lung_roi.Copy;
+    lung_roi.ChangeRawImage(int16(lung_roi.RawImage).*int16(density_average_mask.RawImage));
+    
+    % Average the mask, in order to determine how many neighbouring voxels
+    % contribute towards the density average of each voxel
+    reporting.ShowProgress('Computing scaling factor');
+    number_of_contributing_voxels = convn(single(density_average_mask.RawImage), density_average_el, 'same');
+    
+    % Compute the density at each voxel
+    reporting.ShowProgress('Computing average lung density');
+    density_g_mL_image = TDConvertCTToDensity(lung_roi);
+    
+    % To prevent divide by zero errors
+    zero_mask = number_of_contributing_voxels == 0;
+    number_of_contributing_voxels(zero_mask) = 1;
+
+    % Average the density over the neighbourhood
+    density_average_raw = convn(density_g_mL_image.RawImage, density_average_el, 'same')./number_of_contributing_voxels;
+    
+    % Exclude voxels with more than half of their voxels outside of the lung mask
+    too_few_neighbours = number_of_contributing_voxels < ceil(size_el/2);
+    density_average_mask.ChangeRawImage((density_average_mask.RawImage) & (~too_few_neighbours));
+    density_average_raw(~density_average_mask.RawImage) = 0;
+        
+    reporting.ShowProgress('Storing results');
+    density_average = lung_roi.BlankCopy;
+    density_average.ChangeRawImage(density_average_raw);
+    density_average.ImageType = TDImageType.Scaled;
+end
