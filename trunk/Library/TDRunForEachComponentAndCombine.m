23c020e1ff2df8a10362175098e715fc23c5c657
pulmonarytoolkit@gmail.com <pulmonarytoolkit@gmail.com@032b9d54-5664-1725-390e-cfc7fbea0576>
2012-12-12 21:22:15 +0000

    Added new functions for running algorithms separately on different image regions specified by a mask and recombining the results

---
+++ trunk/Library/TDRunForEachComponentAndCombine.m
@@ -0,0 +1,28 @@
+function results = TDRunForEachComponentAndCombine(function_handle, image_in, mask, reporting)
+    % Separates an image into components according to the mask image, and runs a
+    % given function for each component. The images are then recombined.
+    %
+    % Note: TDRunForEachComponentAndCombineMaskwise is similar, but is used for
+    % running functions directly on the image masks
+    %
+    if islogical(mask.RawImage)
+        results = function_handle(mask);
+    else
+        if ~isinteger(mask.RawImage)
+            reporting.Error('TDRunForEachComponentAndCombine:NonIntegerMask', 'Mask input must be of integer type');
+        end
+        component_range = [max(1, mask.Limits(1)), mask.Limits(2)];
+        first_run = true;
+        for component_index = component_range(1) : component_range(2)
+            masked_image = image_in.GetMaskedImage(mask, component_index);
+            component_result = function_handle(masked_image);
+            if first_run
+                results = component_result;
+                first_run = false;
+            else
+                results.ChangeSubImageWithMask(component_result, mask, component_index);
+            end
+        end
+    end
+end
+
