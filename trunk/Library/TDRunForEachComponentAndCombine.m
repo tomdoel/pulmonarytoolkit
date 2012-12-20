a57145a60fc48801586cc0b06b625feee6e1137a
pulmonarytoolkit@gmail.com <pulmonarytoolkit@gmail.com@032b9d54-5664-1725-390e-cfc7fbea0576>
2012-12-20 23:37:48 +0000

    Comment changes

--- trunk/Library/TDRunForEachComponentAndCombine.m
+++ trunk/Library/TDRunForEachComponentAndCombine.m
@@ -1,10 +1,19 @@
 function results = TDRunForEachComponentAndCombine(function_handle, image_in, mask, reporting)
-    % Separates an image into components according to the mask image, and runs a
-    % given function for each component. The images are then recombined.
+    % TDRunForEachComponentAndCombine. Separates an image into components
+    % according to the mask image, and runs a given function for each component.
+    % The images are then recombined.
     %
     % Note: TDRunForEachComponentAndCombineMaskwise is similar, but is used for
     % running functions directly on the image masks
     %
+    %
+    %     Licence
+    %     -------
+    %     Part of the TD Pulmonary Toolkit. http://code.google.com/p/pulmonarytoolkit
+    %     Author: Tom Doel, 2012.  www.tomdoel.com
+    %     Distributed under the GNU GPL v3 licence. Please see website for details.
+    %
+    
     if islogical(mask.RawImage)
         results = function_handle(mask);
     else
