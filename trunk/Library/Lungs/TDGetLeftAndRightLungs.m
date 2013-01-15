d1f0e59622b4446025d08babf3c23a6a714a732c
pulmonarytoolkit@gmail.com <pulmonarytoolkit@gmail.com@032b9d54-5664-1725-390e-cfc7fbea0576>
2013-01-15 17:18:05 +0000

    TDGetLeftAndRightLungs.m  - added fast mode allowing closing with a smaller structural element

--- trunk/Library/Lungs/TDGetLeftAndRightLungs.m
+++ trunk/Library/Lungs/TDGetLeftAndRightLungs.m
@@ -17,8 +17,14 @@ function results = TDGetLeftAndRightLungs(unclosed_lungs, filtered_threshold_lun
     right_lung.ChangeRawImage(right_lung.RawImage == 1);
     right_lung.CropToFit;
     
+    if TDSoftwareInfo.FastMode
+        close_size = 5;
+    else
+        close_size = 8;
+    end
+    
     % Perform morphological closing with a spherical structure element of radius 8mm
-    right_lung.MorphWithBorder(@imclose, 8);
+    right_lung.MorphWithBorder(@imclose, close_size);
     
     % Fill any remaining holes inside the 3D image
     right_lung = TDFillHolesInImage(right_lung);
@@ -31,7 +37,7 @@ function results = TDGetLeftAndRightLungs(unclosed_lungs, filtered_threshold_lun
     left_lung.CropToFit;
     
     % Perform morphological closing with a spherical structure element of radius 8mm
-    left_lung.MorphWithBorder(@imclose, 8);
+    left_lung.MorphWithBorder(@imclose, close_size);
     % Fill any remaining holes inside the 3D image
     left_lung = TDFillHolesInImage(left_lung);
     
