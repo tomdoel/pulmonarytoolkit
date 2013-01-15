ae076e040b635bfcda958663576cd6d42a0a779b
pulmonarytoolkit@gmail.com <pulmonarytoolkit@gmail.com@032b9d54-5664-1725-390e-cfc7fbea0576>
2013-01-15 17:24:58 +0000

    TDVolume.m  - volume measurement now in a separate function

--- trunk/Plugins/TDVolume.m
+++ trunk/Plugins/TDVolume.m
@@ -36,45 +36,12 @@ classdef TDVolume < TDPlugin
     end
     
     methods (Static)
-        function results = RunPlugin(application, reporting)
-            left_and_right_lungs = application.GetResult('TDLeftAndRightLungs');
-            roi = application.GetResult('TDLungROI');
+        function results = RunPlugin(dataset, reporting)
             
-            left_lung = left_and_right_lungs.RawImage == 2;
-            right_lung = left_and_right_lungs.RawImage == 1;
-            
-            voxel_size = roi.VoxelSize;
-
-            left_results = TDVolume.Analyse(roi, left_lung);
-            right_results = TDVolume.Analyse(roi, right_lung);
-            combined_results = [];
-            combined_results.lung_volume_mm3 = left_results.lung_volume_mm3 + right_results.lung_volume_mm3;
-            combined_results.surface_volume_mm3 = left_results.surface_volume_mm3 + right_results.surface_volume_mm3;
-
-            disp('*****');
-            disp(['  Voxel size: ' num2str(voxel_size(1), '%3.2f') 'mm x ' num2str(voxel_size(2), '%3.2f') 'mm x ' num2str(voxel_size(3), '%3.2f') 'mm']);
-            disp(['  LEFT LUNG: Volume: ' num2str(left_results.lung_volume_mm3/1000, '%7.0f') ' cm^3 error: ' num2str(left_results.surface_volume_mm3/1000, '%7.0f') ' cm^3']);
-            disp(['  RIGHT LUNG: Volume: ' num2str(right_results.lung_volume_mm3/1000, '%7.0f') ' cm^3 error: ' num2str(right_results.surface_volume_mm3/1000, '%7.0f') ' cm^3']);
-            disp(['  BOTH LUNGS: Volume: ' num2str(combined_results.lung_volume_mm3/1000, '%7.0f') ' cm^3 error: ' num2str(combined_results.surface_volume_mm3/1000, '%7.0f') ' cm^3']);            
+            left_and_right_lungs = dataset.GetResult('TDLeftAndRightLungs');
+            TDComputeVolumeFromLungMasks(left_and_right_lungs, reporting);
 
             results = [];
-        end
-    end
-    
-    methods (Static, Access = private)
-        function results = Analyse(lung_image, mask)
-            results = [];
-            
-            voxel_size = lung_image.VoxelSize;
-            results.voxel_size = voxel_size;
-            
-            voxel_volume_mm3 = voxel_size(1) * voxel_size(2) * voxel_size(3);
-            lung_volume_mm3 = sum(mask(:))*voxel_volume_mm3;
-            results.lung_volume_mm3 = lung_volume_mm3;
-            
-            surface = TDGetSurfaceFromSegmentation(mask);
-            surface_volume_mm3 = sum(surface(:))*voxel_volume_mm3;
-            results.surface_volume_mm3 = surface_volume_mm3;
         end
     end
 end
