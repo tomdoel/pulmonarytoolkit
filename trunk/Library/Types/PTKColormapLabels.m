a5f45966fe763ce2d68c50493f4fc23201736ef9
pulmonarytoolkit@gmail.com <pulmonarytoolkit@gmail.com@032b9d54-5664-1725-390e-cfc7fbea0576>
2013-02-19 17:31:52 +0000

    PTKColormapLabels  - Specifies standard colours used in colormap images

---
+++ trunk/Library/Types/PTKColormapLabels.m
@@ -0,0 +1,18 @@
+classdef PTKColormapLabels
+    % PTKColormapLabels. Specifies the labels used for lung regions in colormap images
+    %
+    %
+    %     Licence
+    %     -------
+    %     Part of the TD Pulmonary Toolkit. http://code.google.com/p/pulmonarytoolkit
+    %     Author: Tom Doel, 2012.  www.tomdoel.com
+    %     Distributed under the GNU GPL v3 licence. Please see website for details.
+    %    
+    
+    properties
+        RightLung = 1
+        LeftLung = 2
+    end
+    
+end
+
