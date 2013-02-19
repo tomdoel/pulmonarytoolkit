8f6ac446a57d829aa68a4714399cb5e4b207324c
pulmonarytoolkit <pulmonarytoolkit@gmail.com>
2013-02-19 17:35:00 +0000

    PTKParisePluginClass  - new method to encapsulate plugin information parsing, allowing for older versions of plugins
    PTKPluginInformation
     - changes to use above method and new PTKDirectories class

--- Framework/PTKPluginInformation.m
+++ Framework/PTKPluginInformation.m
@@ -22,8 +22,8 @@ classdef PTKPluginInformation
         
         % Obtains a list of plugins found in the Plugins folder
         function plugin_list = GetListOfPlugins(reporting)
-            plugin_list = PTKDiskUtilities.GetDirectoryFileList(PTKPluginInformation.GetPluginsPath, '*.m');
-            user_plugin_list = PTKDiskUtilities.GetDirectoryFileList(PTKPluginInformation.GetUserPluginsPath, '*.m');
+            plugin_list = PTKDiskUtilities.GetDirectoryFileList(PTKDirectories.GetPluginsPath, '*.m');
+            user_plugin_list = PTKDiskUtilities.GetDirectoryFileList(PTKDirectories.GetUserPluginsPath, '*.m');
             combined_plugin_list = horzcat(plugin_list, user_plugin_list);
             plugin_list = [];
 
@@ -85,43 +85,8 @@ classdef PTKPluginInformation
             plugin_info_structure = feval(plugin_handle);
             
             % Parse the class properties into a data structure
-            new_plugin = PTKPluginInformation.ParsePluginClass(plugin_name, plugin_info_structure, reporting); 
+            new_plugin = PTKParsePluginClass(plugin_name, plugin_info_structure, reporting); 
         end
     end
-    
-    methods (Access = private, Static)
-        
-        function plugins_path = GetPluginsPath
-            full_path = mfilename('fullpath');
-            [path_root, ~, ~] = fileparts(full_path);
-            plugins_path = fullfile(path_root, '..', PTKSoftwareInfo.PluginDirectoryName);
-        end
-        
-        function plugins_path = GetUserPluginsPath
-            full_path = mfilename('fullpath');
-            [path_root, ~, ~] = fileparts(full_path);
-            plugins_path = fullfile(path_root, '..', PTKSoftwareInfo.UserDirectoryName, PTKSoftwareInfo.PluginDirectoryName);
-        end
-        
-        function new_plugin = ParsePluginClass(plugin_name, plugin_class, reporting)
-            new_plugin = [];
-            new_plugin.PluginName = plugin_name;
-            
-            if ~strcmp(plugin_class.PTKVersion, PTKSoftwareInfo.PTKVersion)
-                reporting.ShowWarning('PTKPluginInformation:MismatchingPluginVersion', ['Plugin ' plugin_name ' was created for a more recent version of this software'], []);
-            end
-            
-            new_plugin.ToolTip = plugin_class.ToolTip;
-            new_plugin.AlwaysRunPlugin = plugin_class.AlwaysRunPlugin;
-            new_plugin.AllowResultsToBeCached = plugin_class.AllowResultsToBeCached;
-            new_plugin.PluginType = plugin_class.PluginType;
-            new_plugin.ButtonText = plugin_class.ButtonText;
-            new_plugin.Category = plugin_class.Category;
-            new_plugin.HidePluginInDisplay = plugin_class.HidePluginInDisplay;
-            new_plugin.ButtonWidth = plugin_class.ButtonWidth;
-            new_plugin.ButtonHeight = plugin_class.ButtonHeight;
-        end
-        
-    end    
 end
 
