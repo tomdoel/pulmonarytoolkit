1182543068dbaf025a2819cfca3e633bcb85cc92
pulmonarytoolkit@gmail.com <pulmonarytoolkit@gmail.com@032b9d54-5664-1725-390e-cfc7fbea0576>
2013-02-25 00:52:58 +0000

    PTKSoftwareInfo.m  - Re-ordered some of the properties and moved some functions to PTKDirectories.m

--- trunk/Framework/PTKSoftwareInfo.m
+++ trunk/Framework/PTKSoftwareInfo.m
@@ -16,71 +16,67 @@ classdef PTKSoftwareInfo < handle
     %
 
     properties (Constant)
+        
+        % Version numbers
         Version = '0.2'
         DicomVersion = '0.1'
+        DiskCacheSchema = '0.1'
+        PTKVersion = '2'
+        MatlabMinimumMajorVersion = 7
+        MatlabMinimumMinorVersion = 11
+        MatlabAdvisedMajorVersion = 7
+        MatlabAdvisedMinorVersion = 14
+
+        % Name of application
         Name = 'Pulmonary Toolkit'
         DicomName = 'TD Pulmonary Toolkit'
         DicomManufacturer = 'www tomdoel com'
         DicomStudyDescription = 'TD Pulmonary Toolkit exported images'
+        WebsiteUrl = 'http://code.google.com/p/pulmonarytoolkit'
+
+        % Appearance
+        BackgroundColour = [0, 0.129, 0.278]
+        GraphFont = 'Helvetica'
+        GuiFont = 'Helvetica'
+        
+        % Directories
         DiskCacheFolderName = 'ResultsCache'
         ApplicationSettingsFolderName = 'TDPulmonaryToolkit'
-        DiskCacheSchema = '0.1'
-        LogFileName = 'log.txt'
         PluginDirectoryName = 'Plugins'
         GuiPluginDirectoryName = fullfile('Gui', 'GuiPlugins')
-        PreviewImageFileName = 'PreviewImages'
-        PTKVersion = '1'
+        MexSourceDirectory = fullfile('Library', 'mex')
+        UserDirectoryName = 'User'
+        ResultsDirectoryName = 'Output'
+        TestSourceDirectory = 'Test'
+        
+        % Filenames
+        LogFileName = 'log.txt'
         CachedPluginInfoFileName = 'CachedPluginInfo'
+        PreviewImageFileName = 'PreviewImages'
         SettingsFileName = 'PTKSettings.mat'
         FrameworkCacheFileName = 'PTKFrameworkCache.mat'
         MakerPointsCacheName = 'MarkerPoints'
         ImageInfoCacheName = 'ImageInfo'
         SchemaCacheName = 'Schema'
         ImageTemplatesCacheName = 'ImageTemplates'
-        MexSourceDirectory = fullfile('Library', 'mex')
-        UserDirectoryName = 'User'
-        ResultsDirectoryName = 'Output'
-        WebsiteUrl = 'http://code.google.com/p/pulmonarytoolkit'
-        MatlabMinimumMajorVersion = 7
-        MatlabMinimumMinorVersion = 11
-        MatlabAdvisedMajorVersion = 7
-        MatlabAdvisedMinorVersion = 14
+        SplashScreenImageFile = 'PTKLogo.jpg'
+
+        % Debugging and optional arguments
         DebugMode = false
         GraphicalDebugMode = false
-        BackgroundColour = [0, 0.129, 0.278]
-        CancelErrorId = 'PTKMain:UserCancel'
         TimeFunctions = true;
         FastMode = false;
-        GraphFont = 'Helvetica'
-        GuiFont = 'Helvetica'
-        SplashScreenImageFile = 'PTKLogo.jpg'
+        
+        % Do not change this
+        CancelErrorId = 'PTKMain:UserCancel'        
     end
 
     methods (Static)
-        function application_directory = GetApplicationDirectoryAndCreateIfNecessary
-            if ~isempty(PTKConfig.CacheFolder)
-                home_directory = PTKConfig.CacheFolder;
-            else
-                home_directory = PTKDiskUtilities.GetUserDirectory;
-            end
-            application_directory = PTKSoftwareInfo.ApplicationSettingsFolderName;
-            application_directory = fullfile(home_directory, application_directory);  
-            if ~exist(application_directory, 'dir')
-                mkdir(application_directory);
-            end
-        end
-
         function [major_version, minor_version] = GetMatlabVersion
             [matlab_version, ~] = version;
             version_matrix = sscanf(matlab_version, '%d.%d.%d.%d');
             major_version = version_matrix(1);
             minor_version = version_matrix(2);
-        end
-        
-        function source_directory = GetSourceDirectory
-            full_path = mfilename('fullpath');
-            [path_root, ~, ~] = fileparts(full_path);
-            source_directory = fullfile(path_root, '..');
         end
         
         function is_cancel_id = IsErrorCancel(error_id)
