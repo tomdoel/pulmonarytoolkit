23a360418405d86f457b3a745265afde4d90201e
pulmonarytoolkit@gmail.com <pulmonarytoolkit@gmail.com@032b9d54-5664-1725-390e-cfc7fbea0576>
2013-03-28 11:12:40 +0000

    PTKFileGrouper PTKFileGrouping  - new classes for grouping Dicom files (specified by metadata) into coherent sequences
    PTKSortImagesByLocation
     - Method for sorting a group of Dicom metadata by the slice location

---
+++ trunk/Library/File/PTKFileGrouping.m
@@ -0,0 +1,46 @@
+classdef PTKFileGrouping < handle
+    % PTKFileGrouping. Stores a set of Dicom metadata structures, corresponding
+    %     to a coherent sequence of images
+    %
+    %     PTKFileGrouping objects are created by the PTKFileGrouper class,
+    %     which separates and groups images into coherent sequences according to
+    %     their metadata.
+    %        
+    %
+    %     Licence
+    %     -------
+    %     Part of the TD Pulmonary Toolkit. http://code.google.com/p/pulmonarytoolkit
+    %     Author: Tom Doel, 2013.  www.tomdoel.com
+    %     Distributed under the GNU GPL v3 licence. Please see website for details.
+    %    
+        
+    properties (SetAccess = private)
+        Metadata
+    end
+    
+    methods
+        function obj = PTKFileGrouping(metadata)
+            if ~isempty(metadata)
+                obj.Metadata{1} = metadata;
+            end
+        end
+        
+        function AddFile(obj, metadata)
+            obj.Metadata{end + 1} = metadata;
+        end
+        
+        % Determine if a file with specified metadata should be grouped with
+        % these files
+        function match = Matches(obj, other_metadata)
+            match = PTKAreImagesInSameGroup(obj.Metadata{1}, other_metadata);
+        end
+
+        % Sorts the images according to slice location, and computes values for
+        % slice thickness and global origin
+        function [slice_thickness, global_origin_mm] = SortAndGetParameters(obj, reporting)
+            [sorted_indices, slice_thickness, global_origin_mm] = PTKSortImagesByLocation(obj, reporting);
+            obj.Metadata = obj.Metadata(sorted_indices);
+        end
+    end
+end
+
---
+++ trunk/Library/File/PTKSortImagesByLocation.m
@@ -0,0 +1,120 @@
+function [sorted_indices, slice_thickness, global_origin_mm] = PTKSortImagesByLocation(metadata_grouping, reporting)
+    % PTKSortImagesByLocation. Sorts a series of Dicom images by their slice
+    %     locations and calculates additional image parameters
+    %
+    %     Syntax:
+    %         [sorted_indices, slice_thickness, global_origin_mm] = PTKSortImagesByLocation(metadata_grouping, reporting)
+    %
+    %     Inputs:
+    %         metadata_grouping - A PTKFileGrouping object containing the
+    %             metadata from the group of Dicom images to be sorted
+    %
+    %         reporting - A PTKReporting or implementor of the same interface,
+    %             for error and progress reporting. 
+    %
+    %     Outputs:
+    %         sorted_indices - The indices of the metadata structures in
+    %             metadata_grouping, ordered by slice location
+    %
+    %         slice_thickness - A typical slice thickness for this group of
+    %             images
+    %
+    %         global_origin_mm - The coordinates of the first voxel in the image
+    %             volume
+    %        
+    %
+    %     Licence
+    %     -------
+    %     Part of the TD Pulmonary Toolkit. http://code.google.com/p/pulmonarytoolkit
+    %     Author: Tom Doel, 2013.  www.tomdoel.com
+    %     Distributed under the GNU GPL v3 licence. Please see website for details.
+    %    
+    
+    
+    reporting.ShowProgress('Ordering images');
+    reporting.UpdateProgressValue(0);
+    
+    % Determine if ImagePositionPatient and SliceLocation tags exist. 
+    % It is sufficient to only check the metadata for one image, assuming that
+    % the metadata was grouped prior to calling this function. The grouping
+    % ensures that within a group, each of these tags exist for either all or no
+    % images.
+    representative_metadata = metadata_grouping.Metadata{1};
+    if isfield(representative_metadata, 'ImagePositionPatient')
+        image_positions_patient = PTKContainerUtilities.GetMatrixOfFieldValuesFromSet(metadata_grouping.Metadata, 'ImagePositionPatient')';        
+    else
+        image_positions_patient = [];
+    end
+
+    if isfield(representative_metadata, 'SliceLocation')
+        slice_locations = PTKContainerUtilities.GetMatrixOfFieldValuesFromSet(metadata_grouping.Metadata, 'SliceLocation')';
+    else
+        slice_locations = [];
+    end
+    
+    if isfield(representative_metadata, 'InstanceNumber')
+        instance_numbers = PTKContainerUtilities.GetMatrixOfFieldValuesFromSet(metadata_grouping.Metadata, 'InstanceNumber')';
+    else
+        instance_numbers = [];
+    end
+    
+    % We try to calculate the slice locations from the ImagePositionPatient tags
+    if ~isempty(image_positions_patient) && isfield(representative_metadata, 'ImageOrientationPatient')
+        i_axis = representative_metadata.ImageOrientationPatient(1:3);
+        j_axis = representative_metadata.ImageOrientationPatient(4:6);
+        
+        [~, i_direction] = max(i_axis);
+        [~, j_direction] = max(j_axis);
+        
+        k_direction = setdiff([1, 2, 3], [i_direction, j_direction]);
+        
+        [sorted_slice_locations, sorted_indices] = sort(image_positions_patient(:, k_direction), 'ascend');
+        global_origin_mm = min(image_positions_patient, [], 1);
+        slice_thicknesses = abs(sorted_slice_locations(2:end) - sorted_slice_locations(1:end-1));
+        
+        
+    % If this tag is not present, we try the SliceLocation tags
+    elseif ~isempty(slice_locations)    
+        [sorted_slice_locations, sorted_indices] = sort(slice_locations, 'ascend');
+        global_origin_mm = [];
+        slice_thicknesses = abs(sorted_slice_locations(2:end) - sorted_slice_locations(1:end-1));
+    
+    % In the absense of the above tags, we sort by the instance number (slice
+    % number). Ths is less reliable
+    elseif ~isempty(instance_numbers)
+        sorted_indices = sort(instance_numbers, 'ascend');
+        global_origin_mm = [];
+        slice_thicknesses = [];
+        
+    % Otherwise we set everything to empty
+    else
+        sorted_indices = [];
+        global_origin_mm = [];
+        slice_thicknesses = [];
+    end
+    
+    % Remove any zero thicknesses, which may indicate multiple slices at the
+    % same position
+    if any(slice_thicknesses < 0.01)
+        reporting.ShowWarning('PTKSortImagesByLocation:ZeroSliceThickness', 'This image contains more than one image at the same slice position', []);
+        slice_thicknesses = slice_thicknesses(slice_thicknesses > 0.01);
+    end
+    
+    % If we have no non-zero slice thicknesses (including the case where we only
+    % have a single slice) then try and use the SliceThickness tag if it exists.
+    % Otherwise we just set to empty to indicate that we cannot determine this
+    if isempty(slice_thicknesses)
+        if isfield(metadata_grouping.Metadata{1}, 'SliceThickness')
+            slice_thickness = metadata_grouping.Metadata{1}.SliceThickness;
+        else
+            slice_thickness = [];
+        end
+    else
+        slice_thickness = mode(slice_thicknesses);
+        if any(slice_thicknesses - slice_thickness) > 0.01
+            reporting.ShowWarning('PTKSortImagesByLocation:InconsistentSliceThickness', 'Not all slices have the same thickness', []);
+        end
+    end
+    
+    reporting.CompleteProgress;
+end
