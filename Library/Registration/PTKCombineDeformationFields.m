c6100ffce38bd59820ce1c3698398bb834dd0382
pulmonarytoolkit <pulmonarytoolkit@gmail.com>
2013-05-22 20:03:28 +0000

    PTKCombineDeformationFields  - Ensure the whole deformation field is set - previously it was masked to the registered left and right lungs, which meant the remaining areas of the field were zero, which could actually map to an undesired area of the image. Now we extrapolate the registered left and right lung masks to cover the entire deformation field volume, so that every voxel is mapped using one of the deformation fields.

--- Library/Registration/PTKCombineDeformationFields.m
+++ Library/Registration/PTKCombineDeformationFields.m
@@ -18,15 +18,44 @@ function deformation_field_combined = PTKCombineDeformationFields(deformation_le
         left_right_mask.Resample(deformation_left.VoxelSize, '*nearest');
     end
     
+    % Create a combined deformation field
     deformation_field_combined = left_right_mask.BlankCopy;
     deformation_field_combined.ChangeRawImage(zeros([deformation_field_combined.ImageSize, 3], 'double'));
+
+    % Create a mask of the deformation regions which have been set
+    has_been_set_mask = left_right_mask.BlankCopy;
+    has_been_set_mask.ChangeRawImage(false([left_right_mask.ImageSize]));
+    
+    % Set the deformations for the left lung
     region_mask = left_right_mask.BlankCopy;
     region_mask.ChangeRawImage(left_right_mask.RawImage == 1);
     region_mask.ResizeToMatch(deformation_right);
     deformation_field_combined.ChangeSubImageWithMask(deformation_right, region_mask);
+    has_been_set_mask.ChangeSubImageWithMask(region_mask, region_mask);
+    
+    % Set the deformations for the right lung
     region_mask = left_right_mask.BlankCopy;
     region_mask.ChangeRawImage(left_right_mask.RawImage == 2);
     region_mask.ResizeToMatch(deformation_left);
     deformation_field_combined.ChangeSubImageWithMask(deformation_left, region_mask);
+    has_been_set_mask.ChangeSubImageWithMask(region_mask, region_mask);
+    
+    % Fill in the unset regions using the deformation values of the nearest
+    % points
+    [~, nn_index] = bwdist(has_been_set_mask.RawImage);
+    nn_deformation_field = left_right_mask.BlankCopy;
+    nn_deformation_field_raw = zeros([deformation_field_combined.ImageSize], 'double');
+    
+    deformation_field_combined_component = deformation_field_combined.RawImage(:, :, :, 1);
+    nn_deformation_field_raw(:, :, :, 1) = deformation_field_combined_component(nn_index);
+    deformation_field_combined_component = deformation_field_combined.RawImage(:, :, :, 2);
+    nn_deformation_field_raw(:, :, :, 2) = deformation_field_combined_component(nn_index);
+    deformation_field_combined_component = deformation_field_combined.RawImage(:, :, :, 3);
+    nn_deformation_field_raw(:, :, :, 3) = deformation_field_combined_component(nn_index);
+    
+    nn_deformation_field.ChangeRawImage(nn_deformation_field_raw);
+    has_been_set_mask.ChangeRawImage(~has_been_set_mask.RawImage);
+    deformation_field_combined.ChangeSubImageWithMask(nn_deformation_field, has_been_set_mask);
+    
 end
 
