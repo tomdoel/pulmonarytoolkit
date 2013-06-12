042665db390f73a50cbe7739979d7fabf60118a7
pulmonarytoolkit@gmail.com <pulmonarytoolkit@gmail.com@032b9d54-5664-1725-390e-cfc7fbea0576>
2013-06-12 11:19:51 +0000

    PTKGetSurfaceFromSegmentation.m  - added support for ignoring the coronal direction when computing the surface (useful for thick slice data)

--- trunk/Library/PTKGetSurfaceFromSegmentation.m
+++ trunk/Library/PTKGetSurfaceFromSegmentation.m
@@ -1,4 +1,4 @@
-function segmentation = PTKGetSurfaceFromSegmentation(segmentation)
+function segmentation = PTKGetSurfaceFromSegmentation(segmentation, exclude_direction)
     % PTKGetSurfaceFromSegmentation. Finds the surface of a segmented 3D binary volume.
     %
     % The input and output images are raw image matrices containing a mask of 0s
@@ -11,13 +11,23 @@ function segmentation = PTKGetSurfaceFromSegmentation(segmentation)
     %     Author: Tom Doel, 2012.  www.tomdoel.com
     %     Distributed under the GNU GPL v3 licence. Please see website for details.
     %
-
     
-    segmentation = int8(segmentation == 1);
+    if nargin < 2
+        exclude_direction = [];
+    end
+    
+    segmentation = int8(segmentation > 0);
 
     % Find voxels that are nonzero and do not have 6 nonzero neighbours
     filter = zeros(3, 3, 3);
-    filter(:) = [0 0 0 0 1 0 0 0 0 0 1 0 1 0 1 0 1 0 0 0 0 0 1 0 0 0 0];
+    if isempty(exclude_direction)
+        filter(:) = [0 0 0 0 1 0 0 0 0 0 1 0 1 0 1 0 1 0 0 0 0 0 1 0 0 0 0];
+    elseif exclude_direction == PTKImageOrientation.Coronal
+        filter(:) = [0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0];
+    else
+        error('PTKGetSurfaceFromSegmentation:UnsupportedExcludeDirection', 'Unsupported exclude direction');
+    end
+    max_neighbours = sum(filter(:));
     image_conv = convn(segmentation, filter, 'same');
-    segmentation = (segmentation & (image_conv < 6));
+    segmentation = (segmentation & (image_conv < max_neighbours));
 end
