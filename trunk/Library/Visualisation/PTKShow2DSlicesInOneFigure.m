d15f13ff9b06c54c99476ce5cc610bfb3a9c7c1d
pulmonarytoolkit@gmail.com <pulmonarytoolkit@gmail.com@032b9d54-5664-1725-390e-cfc7fbea0576>
2013-08-17 13:26:03 +0000

    PTKShow2DSlicesInOneFigure  - added setting of units to pixels

--- trunk/Library/Visualisation/PTKShow2DSlicesInOneFigure.m
+++ trunk/Library/Visualisation/PTKShow2DSlicesInOneFigure.m
@@ -75,7 +75,7 @@ function figure_handle = PTKShow2DSlicesInOneFigure(viewer_panel_handle, orienta
     display_height = display_dimensions(2) - 300;
     proportional_width = display_height*(size(im,2)/size(im,1))*(num_plots_x/num_plots_y)*(1 - 2*gap*num_plots_y)/(1 - 2*gap*num_plots_x);
     
-    set(figure_handle, 'Position', [0 0 proportional_width, display_height]);
+    set(figure_handle, 'Units', 'pixels', 'Position', [0 0 proportional_width, display_height]);
     set(figure_handle, 'PaperPositionMode', 'auto');
 end
 
