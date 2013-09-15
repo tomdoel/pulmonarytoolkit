893316beaf3361c656ba8cd4da25873a5900b6ae
pulmonarytoolkit <pulmonarytoolkit@gmail.com>
2013-09-15 12:50:04 +0000

    PTKTreeUtilities  - added utility functions GetCentrelineVoxelsForTheseBranches() and GetCentrelineVoxelsForTheseBranchesExtended()
    PTKColourBranchesByLobe
     - uses above new functions

--- Library/Airways/PTKColourBranchesByLobe.m
+++ Library/Airways/PTKColourBranchesByLobe.m
@@ -18,25 +18,25 @@ function results_image = PTKColourBranchesByLobe(start_branches, airway_tree, te
     uncertain_segments = start_branches.LeftUncertain;
 
     % Get voxels for lobes
-    left_lower_voxels = GetVoxelsForTheseBranches(left_lower_startindices, template);
-    left_upper_voxels = GetVoxelsForTheseBranches(left_upper_startindices, template);
-    right_lower_voxels = GetVoxelsForTheseBranches(right_lower_startindices, template);
-    right_mid_voxels = GetVoxelsForTheseBranches(right_mid_startindices, template);
-    right_upper_voxels = GetVoxelsForTheseBranches(right_upper_startindices, template);
+    left_lower_voxels = PTKTreeUtilities.GetCentrelineVoxelsForTheseBranches(left_lower_startindices, template);
+    left_upper_voxels = PTKTreeUtilities.GetCentrelineVoxelsForTheseBranches(left_upper_startindices, template);
+    right_lower_voxels = PTKTreeUtilities.GetCentrelineVoxelsForTheseBranches(right_lower_startindices, template);
+    right_mid_voxels = PTKTreeUtilities.GetCentrelineVoxelsForTheseBranches(right_mid_startindices, template);
+    right_upper_voxels = PTKTreeUtilities.GetCentrelineVoxelsForTheseBranches(right_upper_startindices, template);
     
-    uncertain_voxels = GetVoxelsForTheseBranches(uncertain_segments, template);
+    uncertain_voxels = PTKTreeUtilities.GetCentrelineVoxelsForTheseBranches(uncertain_segments, template);
 
-    left_lower_voxels_extended = GetVoxelsForTheseBranchesExtended(left_lower_startindices, template);
-    left_upper_voxels_extended = GetVoxelsForTheseBranchesExtended(left_upper_startindices, template);
-    right_lower_voxels_extended = GetVoxelsForTheseBranchesExtended(right_lower_startindices, template);
-    right_mid_voxels_extended = GetVoxelsForTheseBranchesExtended(right_mid_startindices, template);
-    right_upper_voxels_extended = GetVoxelsForTheseBranchesExtended(right_upper_startindices, template);
+    left_lower_voxels_extended = PTKTreeUtilities.GetCentrelineVoxelsForTheseBranchesExtended(left_lower_startindices, template);
+    left_upper_voxels_extended = PTKTreeUtilities.GetCentrelineVoxelsForTheseBranchesExtended(left_upper_startindices, template);
+    right_lower_voxels_extended = PTKTreeUtilities.GetCentrelineVoxelsForTheseBranchesExtended(right_lower_startindices, template);
+    right_mid_voxels_extended = PTKTreeUtilities.GetCentrelineVoxelsForTheseBranchesExtended(right_mid_startindices, template);
+    right_upper_voxels_extended = PTKTreeUtilities.GetCentrelineVoxelsForTheseBranchesExtended(right_upper_startindices, template);
     
-    uncertain_voxels_extended = GetVoxelsForTheseBranchesExtended(uncertain_segments, template);
+    uncertain_voxels_extended = PTKTreeUtilities.GetCentrelineVoxelsForTheseBranchesExtended(uncertain_segments, template);
     
     results_image = zeros(template.ImageSize, 'uint8');
     
-    full_tree_voxels = CentrelinePointsToLocalIndices(start_segment.GetCentrelineTree, template);
+    full_tree_voxels = PTKTreeUtilities.CentrelinePointsToLocalIndices(start_segment.GetCentrelineTree, template);
     
     results_image(full_tree_voxels) = 7;
     
@@ -86,45 +86,4 @@ function results_image = PTKColourBranchesByLobe(start_branches, airway_tree, te
         
         segments_to_do = [segments_to_do segment.Children];
     end
-    
 end
-
-function voxels = GetVoxelsForTheseBranches(start_indices, template)
-    voxels = [];
-    for index = 1 : numel(start_indices)
-        voxels = cat(2, voxels, CentrelinePointsToLocalIndices(start_indices(index).GetCentrelineTree, template));
-    end
-end
-
-    
-function voxels = GetVoxelsForTheseBranchesExtended(start_indices, template)
-    voxels = [];
-    if isempty(start_indices)
-        return;
-    end
-    
-    for index = 1 : numel(start_indices)
-        voxels = cat(2, voxels, CentrelinePointsToLocalIndices(start_indices(index).GetCentrelineTree, template));
-        parent = start_indices(index).Parent;
-        while ~isempty(parent)
-            centreline_indices = CentrelinePointsToLocalIndices(parent.Centreline, template);
-            voxels = cat(2, voxels, centreline_indices);
-            parent = parent.Parent;
-        end
-    end
-
-    % Add nearest neighbours to the list of voxels, otherwise it is possible for
-    % a diagnoally-connected centreline segment to pass through a
-    % diagnoally-connected airway segment
-    [~, linear_offsets27] = PTKImageCoordinateUtilities.GetLinearOffsets(template.ImageSize);
-    voxels = repmat(int32(voxels), 27, 1) + repmat(int32(linear_offsets27'), 1, length(voxels));    
-    voxels = unique(voxels(:));
-end
-
-
-
-function centreline_indices_local = CentrelinePointsToLocalIndices(centreline_points, template_image)
-    centreline_indices_global = [centreline_points.GlobalIndex];
-    centreline_indices_local = template_image.GlobalToLocalIndices(centreline_indices_global);
-end
-
