64d382771e1bda6e305a33acdb523eb22338ac8f
pulmonarytoolkit <pulmonarytoolkit@gmail.com>
2013-09-17 12:20:12 +0000

    PTKDropDownLoadMenuManager PTKGui  - Added a "no dataset" option to the drop-down menu  - Some tidying of the drop-down menu code

--- Gui/PTKDropDownLoadMenuManager.m
+++ Gui/PTKDropDownLoadMenuManager.m
@@ -23,6 +23,7 @@ classdef PTKDropDownLoadMenuManager < handle
     properties (Access = private)
         MenuHandle
         Settings
+        SortedInfoList
     end
     
     methods
@@ -35,9 +36,76 @@ classdef PTKDropDownLoadMenuManager < handle
         function UpdateQuickLoadMenu(obj)
             prev_infos = obj.Settings.PreviousImageInfos;
             if isempty(prev_infos)
-                return
+                sorted_infos = [];
+                sorted_paths = [];
+            else
+                prev_paths = PTKDropDownLoadMenuManager.GetListOfPaths(prev_infos);
+                
+                [~, sorted_indices] = PTKTextUtilities.SortFilenames(prev_paths);
+                sorted_paths = prev_paths(sorted_indices);
+                sorted_infos = prev_infos.values;
+                sorted_infos = sorted_infos(sorted_indices);
             end
             
+            obj.SortedInfoList = sorted_infos;
+            
+            obj.SetMenuText(sorted_paths);
+
+            obj.SelectCurrentlyLoadedDataset;
+        end
+        
+        function image_info = GetImageInfoForIndex(obj, index)
+            previous_infos = obj.SortedInfoList;
+            if isempty(previous_infos) || (index == 1)
+                image_info = [];
+            else
+                image_info = previous_infos{index - 1};
+            end
+        end
+    end
+    
+    methods (Access = private)
+        
+        function SetMenuText(obj, sorted_paths)
+            popupmenu_argument = 'No dataset';
+            for index = 1 : length(sorted_paths)
+                popupmenu_argument = [popupmenu_argument '|' sorted_paths{index}];
+            end
+            set(obj.MenuHandle, 'String', popupmenu_argument);
+        end
+        
+        function SelectCurrentlyLoadedDataset(obj)
+            % Ensure the currently loaded dataset is selected
+            loaded_index = obj.GetCurrentlyLoadedInfoIndex;            
+            if isempty(loaded_index)
+                loaded_index = 1;
+            else
+                loaded_index = loaded_index + 1;
+            end
+            
+            current_index = get(obj.MenuHandle, 'Value');
+            if (current_index ~= loaded_index)
+                set(obj.MenuHandle, 'Value', loaded_index);
+            end
+        end
+        
+        % Returns the index of the drop down menu item which corresponds to the
+        % currently loaded dataset. An empty value means 'No Dataset'.
+        function current_index = GetCurrentlyLoadedInfoIndex(obj)
+            current_index = [];
+            uids = PTKContainerUtilities.GetFieldValuesFromSet(obj.SortedInfoList, 'ImageUid');
+            if ~isempty(obj.Settings.ImageInfo) && ~isempty(obj.Settings.ImageInfo.ImageUid)
+                current_uid = obj.Settings.ImageInfo.ImageUid;
+                current_index = find(ismember(uids, current_uid), 1);
+            end
+        end
+        
+    end
+    
+    methods (Static, Access = private)
+        
+        % Constructs a list of paths from a list of PTKImageInfos.
+        function prev_paths = GetListOfPaths(prev_infos)
             infos = prev_infos.values;
             prev_paths = [];
             for index = 1 : length(infos)
@@ -50,43 +118,8 @@ classdef PTKDropDownLoadMenuManager < handle
                     prev_paths{index} = display_path;
                 end
             end
-            
-            popupmenu_argument = prev_paths{1};
-            for i = 2:length(prev_paths)
-                popupmenu_argument = [popupmenu_argument '|' prev_paths{i}];
-            end
-            
-            uids = prev_infos.keys;
-            if ~isempty(obj.Settings.ImageInfo) && ~isempty(obj.Settings.ImageInfo.ImageUid)
-                current_uid = obj.Settings.ImageInfo.ImageUid;
-                current_number = find(ismember(uids, current_uid), 1);
-                set(obj.MenuHandle, 'Value', current_number);
-            else
-                % Ideally we should set the popupmenu to have no selection at
-                % this point, but Matlab does not allow this
-                
-            end
-            set(obj.MenuHandle, 'String', popupmenu_argument);
         end
         
-        function image_info = GetImageInfoForIndex(obj, index)
-            previous_infos = obj.Settings.PreviousImageInfos;
-            if isempty(previous_infos)
-                image_info = [];
-            else
-                values = previous_infos.values;
-                image_info = values{index};
-                image_uid = image_info.ImageUid;
-
-                % If the selected entry is the currenyly loaded dataset then do
-                % nothing
-                if ~isempty(obj.Settings.ImageInfo) && ~isempty(obj.Settings.ImageInfo.ImageUid)
-                    if strcmp(image_uid, obj.Settings.ImageInfo.ImageUid)
-                        image_info = [];
-                    end
-                end
-            end
-        end
     end
     
 end
