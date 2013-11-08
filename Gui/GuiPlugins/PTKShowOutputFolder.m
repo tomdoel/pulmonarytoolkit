393fd89f658dd51df5ffad3b79019f4d1a4fafa2
pulmonarytoolkit <pulmonarytoolkit@gmail.com>
2013-11-08 15:11:20 +0000

    Moved Gui plugin PTKShowOutputFolder to main codebase Moved plugin PTKSaveLobeMesh to main codebase

---
+++ Gui/GuiPlugins/PTKShowOutputFolder.m
@@ -0,0 +1,36 @@
+classdef PTKShowOutputFolder < PTKGuiPlugin
+    % PTKShowOutputFolder. Gui Plugin for opening an explorer/finder window in the
+    % cache folder of the currently visualised dataset
+    %
+    %     You should not use this class within your own code. It is intended to
+    %     be used by the gui of the Pulmonary Toolkit.
+    %
+    %     PTKShowOutputFolder is a Gui Plugin for the TD Pulmonary Toolkit.
+    %
+    %
+    %     Licence
+    %     -------
+    %     Part of the TD Pulmonary Toolkit. http://code.google.com/p/pulmonarytoolkit
+    %     Author: Tom Doel, 2012.  www.tomdoel.com
+    %     Distributed under the GNU GPL v3 licence. Please see website for details.
+    %    
+
+    properties
+        ButtonText = 'Open Output Folder'
+        ToolTip = 'Opens a folder containing the output files for the current dataset'
+        Category = 'File'
+        Visibility = 'Dataset'
+
+        HidePluginInDisplay = false
+        PTKVersion = '1'
+        ButtonWidth = 4
+        ButtonHeight = 1
+    end
+    
+    methods (Static)
+        function RunGuiPlugin(ptk_gui_app)
+            dataset_cache_path = ptk_gui_app.GetOutputPath;
+            PTKDiskUtilities.OpenDirectoryWindow(dataset_cache_path);
+        end
+    end
+end
