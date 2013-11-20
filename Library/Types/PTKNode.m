3117dcde15cb1162d07563a84b8e4a13cd6a5c28
pulmonarytoolkit <pulmonarytoolkit@gmail.com>
2013-11-20 14:27:39 +0000

    PTKNode.m  + new class for storing a node

---
+++ Library/Types/PTKNode.m
@@ -0,0 +1,17 @@
+classdef PTKNode < handle
+    % PTKNode. A class for storing node data
+    %
+    %
+    %     Licence
+    %     -------
+    %     Part of the TD Pulmonary Toolkit. http://code.google.com/p/pulmonarytoolkit
+    %     Author: Tom Doel, 2013.  www.tomdoel.com
+    %     Distributed under the GNU GPL v3 licence. Please see website for details.
+    %    
+
+    properties
+        CentrelinePoint
+        Parent
+        Children
+    end
+end
