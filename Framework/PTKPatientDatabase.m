e71b65b46c85aadc2099d1d34ad6cdc5f6cb9039
pulmonarytoolkit <pulmonarytoolkit@gmail.com>
2013-11-20 13:17:01 +0000

    PTKPatientDatabase PTKPatientDetails PTKSeriesDatabaseInfo TestPatientDatabse  + new files relating to patient browser

---
+++ Framework/PTKPatientDatabase.m
@@ -0,0 +1,45 @@
+classdef PTKPatientDatabase < handle
+    % PTKPatientDatabase. Part of the internal framework of the Pulmonary Toolkit.
+    %
+    %     You should not use this class within your own code. It is intended to
+    %     be used internally within the framework of the Pulmonary Toolkit.
+    %
+    %     Licence
+    %     -------
+    %     Part of the TD Pulmonary Toolkit. http://code.google.com/p/pulmonarytoolkit
+    %     Author: Tom Doel, 2013.  www.tomdoel.com
+    %     Distributed under the GNU GPL v3 licence. Please see website for details.
+    %
+    
+    properties (Access = private)
+        PatientMap
+        Reporting
+    end
+    
+    methods
+        function obj = PTKPatientDatabase(reporting)
+            obj.Reporting = reporting;
+            obj.PatientMap = containers.Map;
+        end
+        
+        function [names, ids] = GetListOfPatientNames(obj)
+            ids = obj.PatientMap.keys;
+            values = obj.PatientMap.values;
+            names = PTKContainerUtilities.GetFieldValuesFromSet(values, 'Name');
+        end
+        
+        function patient_info = GetPatientInfo(obj)
+            patient_info = obj.PatientMap.values;
+        end
+        
+        function AddDataset(obj, dataset_info)
+            patient_id = dataset_info.PatientId;
+            patient_name = dataset_info.PatientName;
+            if ~obj.PatientMap.isKey(patient_id)
+                obj.PatientMap(patient_id) = PTKPatientDetails(patient_id, patient_name);
+            end
+            obj.PatientMap(patient_id).AddDataset(dataset_info);
+        end
+
+    end
+end
---
+++ Framework/PTKPatientDetails.m
@@ -0,0 +1,42 @@
+classdef PTKPatientDetails < handle
+    % PTKPatientDetails. Part of the internal framework of the Pulmonary Toolkit.
+    %
+    %     You should not use this class within your own code. It is intended to
+    %     be used internally within the framework of the Pulmonary Toolkit.
+    %
+    %     Licence
+    %     -------
+    %     Part of the TD Pulmonary Toolkit. http://code.google.com/p/pulmonarytoolkit
+    %     Author: Tom Doel, 2013.  www.tomdoel.com
+    %     Distributed under the GNU GPL v3 licence. Please see website for details.
+    %
+    
+    properties (SetAccess = private)
+        Id
+        Name
+    end
+    
+    properties (Access = private)
+        DatasetMap
+    end
+    
+    methods
+        function obj = PTKPatientDetails(patient_id, name)
+            obj.Id = patient_id;
+            obj.Name = name;
+            obj.DatasetMap = containers.Map;
+        end
+        
+        function obj = AddDataset(obj, dataset_information)
+            obj.DatasetMap(dataset_information.Uid) = dataset_information;
+        end
+        
+        function num_datasets = GetNumberOfDatasets(obj)
+           num_datasets = obj.DatasetMap.length;
+        end
+        
+        function datasets = GetDatasets(obj)
+            datasets = obj.DatasetMap.values;
+        end
+    end
+end
