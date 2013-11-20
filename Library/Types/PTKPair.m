51e3fe411ded44b5bcf50f08c993fe4c41005401
pulmonarytoolkit <pulmonarytoolkit@gmail.com>
2013-11-20 11:11:38 +0000

    PTKPair  - new container object for holding two values

---
+++ Library/Types/PTKPair.m
@@ -0,0 +1,24 @@
+classdef PTKPair
+    % PTKPair. A class for storing two values
+    %
+    %
+    %     Licence
+    %     -------
+    %     Part of the TD Pulmonary Toolkit. http://code.google.com/p/pulmonarytoolkit
+    %     Author: Tom Doel, 2013.  www.tomdoel.com
+    %     Distributed under the GNU GPL v3 licence. Please see website for details.
+    %    
+    
+    properties
+        First
+        Second
+    end
+    
+    methods 
+        function obj = PTKPair(first, second)
+            obj.First = first;
+            obj.Second = second;
+        end
+    end
+end
+
