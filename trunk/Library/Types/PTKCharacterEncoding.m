35daed6a203fc53ee4fcb324e2a76fda582e20a8
pulmonarytoolkit@gmail.com <pulmonarytoolkit@gmail.com@032b9d54-5664-1725-390e-cfc7fbea0576>
2013-12-03 15:36:49 +0000

    Added new types for Dicom file parsing:  - PTKCharacterEncoding  - PTKDicomVRType  - PTKEnding  - PTKTransferSyntax

---
+++ trunk/Library/Types/PTKCharacterEncoding.m
@@ -0,0 +1,17 @@
+classdef PTKCharacterEncoding
+    % PTKCharacterEncoding
+    %
+    %
+    %
+    %     Licence
+    %     -------
+    %     Part of the TD Pulmonary Toolkit. http://code.google.com/p/pulmonarytoolkit
+    %     Author: Tom Doel, 2013.  www.tomdoel.com
+    %     Distributed under the GNU GPL v3 licence. Please see website for details.
+    %    
+
+    enumeration
+        UTF8
+    end
+end
+
---
+++ trunk/Library/Types/PTKDicomVRType.m
@@ -0,0 +1,18 @@
+classdef PTKDicomVRType
+    % PTKDicomVRType
+    %
+    %
+    %
+    %     Licence
+    %     -------
+    %     Part of the TD Pulmonary Toolkit. http://code.google.com/p/pulmonarytoolkit
+    %     Author: Tom Doel, 2013.  www.tomdoel.com
+    %     Distributed under the GNU GPL v3 licence. Please see website for details.
+    %    
+
+    enumeration
+        Implicit
+        Explicit
+    end
+end
+
