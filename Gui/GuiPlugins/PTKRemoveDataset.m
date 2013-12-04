b2083921f3b2543322711cf2347d84a5d9bd6059
pulmonarytoolkit <pulmonarytoolkit@gmail.com>
2013-12-04 23:54:38 +0000

    A       Gui/GuiPlugins/PTKCapture2DImage.m A       Gui/GuiPlugins/PTKCopyUID.m A       Gui/GuiPlugins/PTKRemoveDataset.m  - added plugins for capture, copying UID and removing a dataset

---
+++ Gui/GuiPlugins/PTKRemoveDataset.m
@@ -0,0 +1,41 @@
+classdef PTKRemoveDataset < PTKGuiPlugin
+    % PTKClearDiskCache. Gui Plugin for removing a dataset.
+    %
+    %     You should not use this class within your own code. It is intended to
+    %     be used by the gui of the Pulmonary Toolkit.
+    %
+    %     PTKRemoveDataset is a Gui Plugin for the TD Pulmonary Toolkit. 
+    %     The gui will create a button for the user to run this plugin.
+    %     Running this plugin will delete all results files from the current 
+    %     dataset results cache folder. Certain internal cache files will not be
+    %     removed.
+    %
+    %
+    %     Licence
+    %     -------
+    %     Part of the TD Pulmonary Toolkit. http://code.google.com/p/pulmonarytoolkit
+    %     Author: Tom Doel, 2012.  www.tomdoel.com
+    %     Distributed under the GNU GPL v3 licence. Please see website for details.
+    %    
+    
+    properties
+        ButtonText = 'Delete Dataset'
+        ToolTip = 'Remove this dataset from the Toolkit'
+        Category = 'File'
+        Visibility = 'Dataset'
+
+        HidePluginInDisplay = false
+        PTKVersion = '1'
+        ButtonWidth = 4
+        ButtonHeight = 1
+    end
+    
+    methods (Static)
+        function RunGuiPlugin(ptk_gui_app)
+            % Delete files from the disk cache
+            ptk_gui_app.DeleteImageInfo;
+        end
+    end
+    
+end
+
