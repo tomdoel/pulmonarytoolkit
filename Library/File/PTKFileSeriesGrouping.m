6e77c083ae3c67b2d63a2d0bb5768321c1c8c073
pulmonarytoolkit <pulmonarytoolkit@gmail.com>
2013-12-05 00:02:08 +0000

    A       Library/File/PTKFileSeriesGrouper.m A       Library/File/PTKFileSeriesGrouping.m  - added new helper functions for grouping images by series

---
+++ Library/File/PTKFileSeriesGrouping.m
@@ -0,0 +1,33 @@
+classdef PTKFileSeriesGrouping < handle
+    % PTKFileSeriesGrouping.
+    %
+    %        
+    %
+    %     Licence
+    %     -------
+    %     Part of the TD Pulmonary Toolkit. http://code.google.com/p/pulmonarytoolkit
+    %     Author: Tom Doel, 2013.  www.tomdoel.com
+    %     Distributed under the GNU GPL v3 licence. Please see website for details.
+    %    
+        
+    properties (SetAccess = private)
+        SeriesUid
+        Filenames
+    end
+    
+    methods
+        function obj = PTKFileSeriesGrouping(uid, filename)
+            if nargin > 0
+                obj.SeriesUid = uid;
+                if ~isempty(filename)
+                    obj.Filenames{1} = filename;
+                end
+            end
+        end
+        
+        function AddFile(obj, filename)
+            obj.Filenames{end + 1} = filename;
+        end
+    end
+end
+
