88dcf323b0b4cfe9ee9a05dc45f80eb614a386c0
pulmonarytoolkit@gmail.com <pulmonarytoolkit@gmail.com@032b9d54-5664-1725-390e-cfc7fbea0576>
2013-12-11 22:38:52 +0000

    M       Library/Dicom/PTKGetDicomSeries.m  - fix for checking non-Dicom files during an import.  - Also added a fallback to Matlab's dicominfo parser for files with undefined value lengths, which are not currently supported by the fast header reader

--- trunk/Library/Dicom/PTKGetDicomSeries.m
+++ trunk/Library/Dicom/PTKGetDicomSeries.m
@@ -10,16 +10,29 @@ function dicom_series = PTKGetDicomSeries(file_path, file_name, tags_to_get, rep
     %     Distributed under the GNU GPL v3 licence. Please see website for details.
     %
 
-    if nargin < 3
+    if nargin < 4
         reporting = PTKReportingDefault;
     end
 
     if isempty(tags_to_get)
         tags_to_get = PTKDicomDictionary.GroupingTags;
     end
+
+    try
+        header = PTKFastReadDicomHeader(file_path, file_name, tags_to_get, reporting);
+    catch ex
+        header = dicominfo(fullfile(file_path, file_name));
+    end
     
-    header = PTKFastReadDicomHeader(file_path, file_name, tags_to_get, reporting);
-    if ~isempty(header)
-        dicom_series = header.SeriesInstanceUID;
+    if isempty(header)
+        dicom_series = [];
+    else
+        % If no SeriesInstanceUID tag then this is not a valid Dicom image (it
+        % might be a DICOMDIR)
+        if isfield(header, 'SeriesInstanceUID')
+            dicom_series = header.SeriesInstanceUID;
+        else
+            dicom_series = [];
+        end
     end
 end
