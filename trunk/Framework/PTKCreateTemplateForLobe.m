0a9edeaf5a8397032ad00b28faaf5ced5d0a45ad
pulmonarytoolkit@gmail.com <pulmonarytoolkit@gmail.com@032b9d54-5664-1725-390e-cfc7fbea0576>
2013-12-16 19:40:05 +0000

    A       Framework/PTKCreateTemplateForLobe.m  - function for creating lobe template image

---
+++ trunk/Framework/PTKCreateTemplateForLobe.m
@@ -0,0 +1,13 @@
+function template = PTKCreateTemplateForLobe(lobe_mask, context, reporting)
+    % PTKCreateTemplateForLobe. Function for creating a template image
+    %     for a lobar regon
+    %
+    %     Licence
+    %     -------
+    %     Part of the TD Pulmonary Toolkit. http://code.google.com/p/pulmonarytoolkit
+    %     Author: Tom Doel, 2013.  www.tomdoel.com
+    %     Distributed under the GNU GPL v3 licence. Please see website for details.
+    %
+    
+    template = lobe_mask.Copy;
+end
