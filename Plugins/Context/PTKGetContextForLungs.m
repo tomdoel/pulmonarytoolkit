0e00892f97b366a67080b84498c9087f73466e36
pulmonarytoolkit <pulmonarytoolkit@gmail.com>
2013-12-17 14:58:50 +0000

    A       Framework/PTKCreateTemplateForLungs.m A       Plugins/Context/PTKGetContextForLungs.m  - added functions/plugins for lungs context
    M       Plugins/Context/PTKGetContextForSingleLung.m
     - Documentation change

--- Plugins/Context/PTKGetContextForLungs.m
+++ Plugins/Context/PTKGetContextForLungs.m
@@ -1,5 +1,6 @@
-classdef PTKGetContextForSingleLung < PTKPlugin
-    % PTKGetContextForSingleLung. Plugin for finding the region of interest for the left or right lung.
+classdef PTKGetContextForLungs < PTKPlugin
+    % PTKGetContextForLungs. Plugin for finding the region of interest for the
+    % lungs
     %
     %     This is a plugin for the Pulmonary Toolkit. Plugins can be run using 
     %     the gui, or through the interfaces provided by the Pulmonary Toolkit.
@@ -7,27 +8,23 @@ classdef PTKGetContextForSingleLung < PTKPlugin
     %
     %     Plugins should not be run directly from your code.
     %
-    %     PTKGetContextForSingleLung runs the plugin PTKLeftAndRightLungs to segment
-    %     the left and right lungs, then calls the library function
-    %     PTKGetLungROIFromLeftAndRightLungs to extract the region of interest
-    %     for the left or right lung
     %
     %
     %     Licence
     %     -------
     %     Part of the TD Pulmonary Toolkit. http://code.google.com/p/pulmonarytoolkit
-    %     Author: Tom Doel, 2012.  www.tomdoel.com
+    %     Author: Tom Doel, 2013.  www.tomdoel.com
     %     Distributed under the GNU GPL v3 licence. Please see website for details.
     %    
     
     properties
-        ButtonText = 'Single Lung <BR>ROI'
-        ToolTip = 'Change the context to one lung'
+        ButtonText = 'Lungs <BR>ROI'
+        ToolTip = 'Change the context to the lungs'
         Category = 'Context'
 
         AllowResultsToBeCached = true
         AlwaysRunPlugin = false
-        Context = PTKContextSet.SingleLung
+        Context = PTKContextSet.Lungs
         PluginType = 'ReplaceImage'
         HidePluginInDisplay = true
         FlattenPreviewImage = false
@@ -39,8 +36,8 @@ classdef PTKGetContextForSingleLung < PTKPlugin
     
     methods (Static)
         function results = RunPlugin(dataset, context, reporting)
-            left_and_right_lung_mask = dataset.GetResult('PTKLeftAndRightLungs', PTKContext.LungROI);
-            results = PTKGetLungROIFromLeftAndRightLungs(left_and_right_lung_mask, context, reporting);
+            results = dataset.GetResult('PTKLeftAndRightLungs', PTKContext.LungROI);
+            results.ChangeRawImage(results.RawImage > 0);
         end
     end
 end
