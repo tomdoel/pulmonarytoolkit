0c895508208a03d18ecf847a1d566ad7dbeb691f
pulmonarytoolkit <pulmonarytoolkit@gmail.com>
2013-12-23 12:36:15 +0000

    M       Plugins/Lobes/PTKSegmentsByNearestBronchus.m  - rename of PTKSegmentalBronchiForEachLobe to PTKSegmentalBronchi

--- Plugins/Lobes/PTKSegmentsByNearestBronchus.m
+++ Plugins/Lobes/PTKSegmentsByNearestBronchus.m
@@ -38,7 +38,7 @@ classdef PTKSegmentsByNearestBronchus < PTKPlugin
         function results = RunPlugin(dataset, reporting)
             airway_results = dataset.GetResult('PTKAirways');
             left_and_right_lungs = dataset.GetResult('PTKLeftAndRightLungs');
-            segmental_bronchi_for_lobes = dataset.GetResult('PTKSegmentalBronchiForEachLobe');
+            segmental_bronchi_for_lobes = dataset.GetResult('PTKSegmentalBronchi');
             lobes = dataset.GetResult('PTKLobesFromFissurePlane');
             [segment_image_map, labelled_segments] = PTKGetSegmentsByNearestBronchus(airway_results.AirwayTree, left_and_right_lungs, segmental_bronchi_for_lobes.StartBranches, lobes, reporting);
             results = [];
