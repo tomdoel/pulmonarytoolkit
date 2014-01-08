91c40d9756ad53792f8c0e60a9ed42f80de090d3
pulmonarytoolkit@gmail.com <pulmonarytoolkit@gmail.com@032b9d54-5664-1725-390e-cfc7fbea0576>
2014-01-08 19:12:06 +0000

    M       Library/Types/PTKAirwayGeneratorApex.m  - added support for zero-parameter construction, to enable creation of empty arrays

--- trunk/Library/Types/PTKAirwayGeneratorApex.m
+++ trunk/Library/Types/PTKAirwayGeneratorApex.m
@@ -10,18 +10,20 @@ classdef PTKAirwayGeneratorApex < handle
     %     Author: Tom Doel, 2012.  www.tomdoel.com
     %     Distributed under the GNU GPL v3 licence. Please see website for details.
     %
-
+    
     properties
-        AirwayGrowingTreeSegment   % The terminal tree segment     
+        AirwayGrowingTreeSegment   % The terminal tree segment
         PointCloud                 % The points this tree will grow in to
         IsGrowingApex              % Determines whether this apex can grow
     end
     
     methods
         function obj = PTKAirwayGeneratorApex(tree_segment, point_cloud, is_growing)
-            obj.AirwayGrowingTreeSegment = tree_segment;
-            obj.PointCloud = point_cloud;
-            obj.IsGrowingApex = is_growing;
+            if nargin > 0
+                obj.AirwayGrowingTreeSegment = tree_segment;
+                obj.PointCloud = point_cloud;
+                obj.IsGrowingApex = is_growing;
+            end
         end
     end
 end
