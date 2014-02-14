d1a7fa6f0e2938e8a8d9d05c3db9e9e56fd52f28
pulmonarytoolkit <pulmonarytoolkit@gmail.com>
2014-02-14 00:36:05 +0000

    M       Library/Utilities/PTKGraphUtilities.m  - Improved GetOptimalTickSpacing(), plus it now computes minimum and maximum tick marks

--- Library/Utilities/PTKGraphUtilities.m
+++ Library/Utilities/PTKGraphUtilities.m
@@ -11,16 +11,35 @@ classdef PTKGraphUtilities
     
     methods (Static)
 
-        function tick_spacing = GetOptimalTickSpacing(min_value, max_value)
+        function [tick_spacing, min_value, max_value] = GetOptimalTickSpacing(min_value, max_value)
             % Returns the tick spacing required that will look good on printed graphs
             
             range = max_value - min_value;
             
             power_of_10 = log10(range);
             frac = power_of_10 - floor(power_of_10);
-            power_of_10 = floor(power_of_10) + (frac > 0.7) - 1;
-            tick_spacing = range / 5;
-            tick_spacing = (10^power_of_10)*round(tick_spacing/(10^power_of_10)); 
+            power_of_10 = floor(power_of_10);
+            
+            % For fractional values lower than about 3, we lower the power of 10
+            if frac < 0.477
+                power_of_10 = power_of_10 - 1;
+                if frac < 0.1
+                    tick_multiplier = 2;
+                else
+                    tick_multiplier = 5;
+                end
+            else
+                if frac > 0.7
+                    tick_multiplier = 2;
+                else
+                    tick_multiplier = 1;
+                end
+            end
+            
+            tick_spacing = tick_multiplier*(10^power_of_10);
+            
+            min_value = tick_spacing*(floor(min_value/tick_spacing + 0.1));
+            max_value = tick_spacing*(ceil(max_value/tick_spacing - 0.1));
         end
         
     end
