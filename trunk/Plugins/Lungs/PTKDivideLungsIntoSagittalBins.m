b2d4de7ea8081fd2ba6e7bd2fec8edd5c506ae0c
pulmonarytoolkit@gmail.com <pulmonarytoolkit@gmail.com@032b9d54-5664-1725-390e-cfc7fbea0576>
2014-02-21 23:04:33 +0000

    M       Plugins/Analysis/PTKSpongeModel.m M       Plugins/Lungs/PTKDivideLungsIntoAxialBins.m M       Plugins/Lungs/PTKDivideLungsIntoCoronalBins.m M       Plugins/Lungs/PTKDivideLungsIntoSagittalBins.m  - Now set the viewer orientation during plugin execution

--- trunk/Plugins/Lungs/PTKDivideLungsIntoSagittalBins.m
+++ trunk/Plugins/Lungs/PTKDivideLungsIntoSagittalBins.m
@@ -39,6 +39,7 @@ classdef PTKDivideLungsIntoSagittalBins < PTKPlugin
             whole_lung_mask = dataset.GetTemplateImage(PTKContext.Lungs);
             orientation = PTKImageOrientation.Sagittal;            
             results = PTKDivideVolumeIntoSlices(whole_lung_mask, orientation, reporting);
+            reporting.ChangeViewingOrientation(PTKImageOrientation.Coronal);
         end
         
         function results = GenerateImageFromResults(bins_results, image_templates, reporting)
