f6384e1fdcc0e47a1e328866752a658e20f56e3d
pulmonarytoolkit@gmail.com <pulmonarytoolkit@gmail.com@032b9d54-5664-1725-390e-cfc7fbea0576>
2014-04-02 15:09:37 +0000

    A       Library/File/PTKLoadImages.m M       Plugins/Context/PTKOriginalImage.m  - Moved some loading code from PTKOriginalImage into PTKLoadImages

---
+++ trunk/Library/File/PTKLoadImages.m
@@ -0,0 +1,46 @@
+function image = PTKLoadImages(image_info, reporting)
+    % PTKLoadImages.
+    %
+    %
+    %     Licence
+    %     -------
+    %     Part of the TD Pulmonary Toolkit. http://code.google.com/p/pulmonarytoolkit
+    %     Author: Tom Doel, 2014.  www.tomdoel.com
+    %     Distributed under the GNU GPL v3 licence. Please see website for details.
+    %
+    
+    
+    
+    image_path = image_info.ImagePath;
+    filenames = image_info.ImageFilenames;
+    image_file_format = image_info.ImageFileFormat;
+    study_uid = image_info.StudyUid;
+    
+    if isempty(filenames)
+        filenames = PTKDiskUtilities.GetDirectoryFileList(image_path, '*');
+        if isempty(filenames)
+            reporting.Error(PTKSoftwareInfo.FileMissingErrorId, ['Cannot find any files in the folder ' image_path]);
+        end
+    else
+        first_file = filenames{1};
+        if isa(first_file, 'PTKFilename')
+            first_file_path = first_file.Path;
+            first_file_name = first_file.Name;
+        else
+            first_file_path = image_path;
+            first_file_name = first_file;
+        end
+        if ~PTKDiskUtilities.FileExists(first_file_path, first_file_name)
+            reporting.Error(PTKSoftwareInfo.FileMissingErrorId, ['Cannot find the file ' fullfile(image_path, filenames{1})]);
+        end
+    end
+    
+    switch image_file_format
+        case PTKImageFileFormat.Dicom
+            image = PTKLoadImageFromDicomFiles(image_path, filenames, reporting);
+        case PTKImageFileFormat.Metaheader
+            image = PTKLoad3DRawAndMetaFiles(image_path, filenames, study_uid, reporting);
+        otherwise
+            reporting.Error('PTKOriginalImage:UnknownImageFileFormat', 'Could not load the image because the file format was not recognised.');
+    end
+end
--- trunk/Plugins/Context/PTKOriginalImage.m
+++ trunk/Plugins/Context/PTKOriginalImage.m
@@ -42,45 +42,8 @@ classdef PTKOriginalImage < PTKPlugin
     methods (Static)
         function results = RunPlugin(dataset, context, reporting)
             reporting.ShowProgress('Loading Images');
-            results = PTKOriginalImage.LoadImages(dataset.GetImageInfo, reporting);
+            results = PTKLoadImages(dataset.GetImageInfo, reporting);
             reporting.CompleteProgress;
         end
-        
-        function image = LoadImages(image_info, reporting)
-            image_path = image_info.ImagePath;
-            filenames = image_info.ImageFilenames;
-            image_file_format = image_info.ImageFileFormat;
-            study_uid = image_info.StudyUid;
-            
-            if isempty(filenames)
-                filenames = PTKDiskUtilities.GetDirectoryFileList(image_path, '*');
-                if isempty(filenames)
-                    reporting.Error(PTKSoftwareInfo.FileMissingErrorId, ['Cannot find any files in the folder ' image_path]);
-                end
-            else
-                first_file = filenames{1};
-                if isa(first_file, 'PTKFilename')
-                    first_file_path = first_file.Path;
-                    first_file_name = first_file.Name;
-                else
-                    first_file_path = image_path;
-                    first_file_name = first_file;
-                end
-                if ~PTKDiskUtilities.FileExists(first_file_path, first_file_name)
-                    reporting.Error(PTKSoftwareInfo.FileMissingErrorId, ['Cannot find the file ' fullfile(image_path, filenames{1})]);
-                end
-            end
-            
-            switch(image_file_format)
-                case PTKImageFileFormat.Dicom
-                    image = PTKLoadImageFromDicomFiles(image_path, filenames, reporting);
-                case PTKImageFileFormat.Metaheader
-                    image = PTKLoad3DRawAndMetaFiles(image_path, filenames, study_uid, reporting);
-                otherwise
-                    reporting.Error('PTKOriginalImage:UnknownImageFileFormat', 'Could not load the image because the file format was not recognised.');
-            end            
-        end
-
     end
-    
 end
