24be53ef00945a9a4c981a4ca912575cdd0f9110
pulmonarytoolkit <pulmonarytoolkit@gmail.com>
2014-08-30 23:36:02 +0000

    M       Library/Analysis/PTKComputeMeanADC.m  - Adjusted standard deviation outputs to be same units as mean

--- Library/Analysis/PTKComputeMeanADC.m
+++ Library/Analysis/PTKComputeMeanADC.m
@@ -14,11 +14,11 @@ function results = PTKComputeMeanADC(adc, region_mask)
     adc_mask = adc.RawImage > 0;
     adc_mask = adc_mask & region_mask.RawImage > 0;
             
-    adc_values = adc.RawImage(adc_mask);
+    adc_values = adc.RawImage(adc_mask(:));
     mean_adc = mean(adc_values);
     std_adc = std(adc_values);
             
     results = PTKMetrics;
     results.AddMetric('MeanADC', mean_adc, 'Mean ADC (cm^2 s^-1)');
-    results.AddMetric('StdADC', std_adc/1000, 'Standard deviation of ADC (cm^2 s^-1)');
+    results.AddMetric('StdADC', std_adc, 'Standard deviation of ADC (cm^2 s^-1)');
 end
