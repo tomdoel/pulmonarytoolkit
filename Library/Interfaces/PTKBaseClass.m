347fd4d9f909a41cfb5016ed843613ade522b422
pulmonarytoolkit <pulmonarytoolkit@gmail.com>
2014-09-09 08:18:52 +0000

    M       Library/Interfaces/PTKBaseClass.m  - Additional null checks

--- Library/Interfaces/PTKBaseClass.m
+++ Library/Interfaces/PTKBaseClass.m
@@ -9,7 +9,7 @@ classdef PTKBaseClass < handle
     %     Author: Tom Doel, 2014.  www.tomdoel.com
     %     Distributed under the GNU GPL v3 licence. Please see website for details.
     %
-
+    
     properties (Access = protected)
         EventListeners
     end
@@ -22,17 +22,19 @@ classdef PTKBaseClass < handle
         end
         
         function delete(obj)
-            for listener = obj.EventListeners
-                delete(listener{1});
-            end
-            obj.EventListeners = [];
-
-            if PTKSoftwareInfo.MonitorClassInstances
-                PTKClassMonitor.GetClassMonitor.ObjectDeleted(class(obj));
+            if isvalid(obj)
+                for listener = obj.EventListeners
+                    delete(listener{1});
+                end
+                obj.EventListeners = [];
+                
+                if PTKSoftwareInfo.MonitorClassInstances
+                    PTKClassMonitor.GetClassMonitor.ObjectDeleted(class(obj));
+                end
             end
         end
-    end 
-       
+    end
+    
     methods (Access = protected)
         
         function AddEventListener(obj, control, event_name, function_handle)
@@ -46,7 +48,7 @@ classdef PTKBaseClass < handle
             % Adds a new event listener tied to the lifetime of this object, for the PostSet
             % operation
             
-            new_listener = addlistener(control, event_name, 'PostSet', function_handle);            
+            new_listener = addlistener(control, event_name, 'PostSet', function_handle);
             obj.EventListeners{end + 1} = new_listener;
         end
         
