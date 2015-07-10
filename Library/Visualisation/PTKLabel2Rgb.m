6e7397647a5c90ae7fe5a7685880572ae644244e
Tom Doel <pulmonarytoolkit@gmail.com>
2015-07-10 12:33:16 +0100

    Replaced call to label2rgb to ind2rgb8. This has the same behaviour provided the label_image is an integer type, and it removes an image processing toolbox dependency

--- Library/Visualisation/PTKLabel2Rgb.m
+++ Library/Visualisation/PTKLabel2Rgb.m
@@ -1,6 +1,6 @@
 function rgb_image = PTKLabel2Rgb(label_image)
     % PTKLabel2Rgb. Converts a label image to an RGB image using the PTK
-    % defauult colorscheme
+    % default colorscheme
     %
     %
     %     Licence
@@ -10,6 +10,6 @@ function rgb_image = PTKLabel2Rgb(label_image)
     %     Distributed under the GNU GPL v3 licence. Please see website for details.
     %
 
-    label_image = 1 + mod(label_image - 1, 63);    
-    rgb_image = label2rgb(label_image, PTKSoftwareInfo.Colormap);
+    label_image = 1 + mod(label_image - 1, 63);
+    rgb_image = ind2rgb8(label_image, [[0, 0, 0]; PTKSoftwareInfo.Colormap]);
 end
