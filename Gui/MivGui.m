e4eb3bbeae83442eaaaf8b03a00d874a03c5f3ac
Tom Doel <pulmonarytoolkit@gmail.com>
2015-07-13 16:41:48 +0100

    Added start script for miv viewer

---
+++ Gui/MivGui.m
@@ -0,0 +1,23 @@
+classdef MivGui < PTKGuiCore
+    % MivGui Main class for the MIV 3D Medical Image Viewer for Matlab
+    %
+    %     To run MIV, type
+    %
+    %         miv
+    %
+    %     at the Matlab command prompt
+    % 
+    %
+    %     Licence
+    %     -------
+    %     Part of the TD Pulmonary Toolkit. https://github.com/tomdoel/pulmonarytoolkit
+    %     Author: Tom Doel, 2015.  www.tomdoel.com
+    %     Distributed under the GNU GPL v3 licence. Please see website for details.
+    %
+    
+    methods
+        function obj = MivGui(splash_screen)
+            obj = obj@PTKGuiCore(splash_screen);
+        end
+    end
+end
---
+++ Gui/MivSplashScreen.m
@@ -0,0 +1,260 @@
+classdef (Sealed) MivSplashScreen < PTKProgressInterface & PTKFigure
+    % MivSplashScreen. A splash screen dialog which also reports progress informaton
+    %
+    %     MivSplashScreen creates a dialog with the application logo and version
+    %     information. It also displays progress information, for use during the
+    %     application startup for reporting progress before the user interface
+    %     is visible.
+    %
+    %     MivSplashScreen is a singleton. You cannot create it using the
+    %     constructor; instead call MivSplashScreen.GetSplashScreen;
+    %
+    %
+    %     Licence
+    %     -------
+    %     Part of the TD Pulmonary Toolkit. https://github.com/tomdoel/pulmonarytoolkit
+    %     Author: Tom Doel, 2012.  www.tomdoel.com
+    %     Distributed under the GNU GPL v3 licence. Please see website for details.
+    %    
+    
+    properties (Access = private)
+        Image
+        TitleText
+        BodyText
+        PanelHandle
+        Disabled = false
+        
+        ProgressBarHandle
+        Text
+        ProgressTitle
+        Cancel
+        Quit
+        UserClickedCancel = false
+
+        IncrementThreshold = 5
+        HandleToWaitDialog
+        DialogText
+        DialogTitle = ''
+        ProgressValue = 0
+        
+        Hold = false;
+        ShowProgressBar = false
+        
+        TimerRef
+        MaxTimeBetweenUpdates = 0.25
+        
+        PanelIsShown = false
+        LastText
+        LastTitle
+    end
+        
+    methods (Static)
+        function splash_screen = GetSplashScreen
+            persistent SplashScreen
+            if isempty(SplashScreen) || ~isvalid(SplashScreen)
+                SplashScreen = MivSplashScreen;
+            end
+            splash_screen = SplashScreen;
+        end
+    end
+    
+    methods (Access = private)
+        function obj = MivSplashScreen
+            
+            % Calculate the figure windows size
+            set(0, 'Units', 'pixels');
+            screen_size = get(0, 'ScreenSize');
+            position = [1, 1, 700, 300];
+            position(1) = max(1, round((screen_size(3) - position(3))/2));
+            position(2) = max(1, round((screen_size(4) - position(4))/2));
+            
+            % Call the base class to initialise the hidden window
+            reporting = PTKReportingDefault;
+            obj = obj@PTKFigure('', position, reporting);            
+            
+            obj.TimerRef = tic;
+            
+            % Hide the progress bar
+            obj.Hide;
+            
+            % Create the figure
+            obj.Show;
+            set(obj.ProgressBarHandle, 'visible', 0);
+            
+            drawnow;
+        end
+    end
+    
+    methods
+        function delete(obj)
+            delete(obj.GraphicalComponentHandle);
+        end        
+
+        function CreateGuiComponent(obj, position)
+            CreateGuiComponent@PTKFigure(obj, position);
+
+            % Override the colour and resize behaviour
+            set(obj.GraphicalComponentHandle, 'Color', [1 1 1], 'Resize', 'off');
+            
+            obj.Image = axes('Parent', obj.GraphicalComponentHandle, 'Units', 'Pixels', 'Position', [30, 70, 223, 200]);
+            logo = imread(PTKSoftwareInfo.SplashScreenImageFile);
+            image(logo, 'Parent', obj.Image);
+            axis(obj.Image, 'off');
+            obj.TitleText = uicontrol('Style', 'text', 'Units', 'Pixels', 'Position', [300, 210, 350, 75], 'String', MivSoftwareInfo.Name, 'FontName', PTKSoftwareInfo.GuiFont, 'FontUnits', 'pixels', 'FontSize', 40, 'FontWeight', 'bold', 'ForegroundColor', [0, 0.129, 0.278], 'BackgroundColor', [1 1 1]);
+            
+            obj.BodyText = uicontrol('Style', 'text', 'Units', 'Pixels', 'Position', [300, 130, 350, 110], 'FontName', PTKSoftwareInfo.GuiFont, 'FontUnits', 'pixels', 'FontSize', 16, 'FontWeight', 'bold', 'ForegroundColor', [0, 0.129, 0.278], 'BackgroundColor', [1 1 1], 'HorizontalAlignment', 'Center');
+            set(obj.BodyText, 'String', sprintf(['Version ' MivSoftwareInfo.Version]));
+            
+            % Create the progress reporting
+            panel_background_colour = [1 1 1];
+            text_color = [0.0 0.129 0.278];
+            
+            title_position = [250, 150, 450, 30];
+            text_position = [250, 90, 450, 60];
+            cancel_position = [400, 20, 140, 30];
+            quit_position = [580, 20, 70, 30];
+            progress_bar_position = [300, 70, 350, 18];
+            
+            
+            obj.ProgressTitle = uicontrol('parent', obj.GraphicalComponentHandle, 'style', 'text', 'units', 'pixel', 'Position', title_position, ...
+                'string', 'Please wait', 'FontUnits', 'pixels', 'FontSize', 22, 'FontWeight', 'bold', 'Fore', text_color, 'Back', panel_background_colour, 'Visible', 'off');
+            obj.Text = uicontrol('parent', obj.GraphicalComponentHandle, 'style', 'text', 'units', 'pixel', 'Position', text_position, ...
+                'string', 'Please wait', 'FontUnits', 'pixels', 'FontSize', 16, 'Fore', text_color, 'Back', panel_background_colour, 'Visible', 'off');
+            obj.Cancel = uicontrol('parent', obj.GraphicalComponentHandle, 'string', 'Cancel', ...
+                'FontUnits', 'pixels', 'Position', cancel_position, 'Visible', 'off', 'Callback', @obj.CancelButton);
+            obj.Quit = uicontrol('parent', obj.GraphicalComponentHandle, 'string', 'Force Quit', ...
+                'FontUnits', 'pixels', 'Position', quit_position, 'Visible', 'off', 'Callback', @obj.QuitButton);
+            
+            [obj.ProgressBarHandle, ~] = javacomponent('javax.swing.JProgressBar', progress_bar_position, obj.GraphicalComponentHandle);
+            obj.ProgressBarHandle.setValue(0);
+            set(obj.ProgressBarHandle, 'visible', 0);
+        end        
+                       
+        function ShowAndHold(obj, text)
+            if nargin < 2
+                text = 'Please wait';
+            end
+            obj.Hide;
+            obj.DialogTitle = PTKTextUtilities.RemoveHtml(text);
+            obj.DialogText = '';
+            obj.ProgressValue = 0;
+            obj.Hold = true;
+            obj.Update;
+            obj.ShowPanel;
+            obj.UserClickedCancel = false;
+        end
+        
+        function Hide(obj)
+            obj.DialogTitle = 'Please wait';
+            obj.ShowProgressBar = false;
+
+            obj.HidePanel;
+            obj.Hold = false;
+        end
+        
+        % Call to complete a progress operaton, which will also hide the dialog
+        % unless the dialog is being held
+        function Complete(obj)
+            obj.ShowProgressBar = false;
+            obj.ProgressValue = 100;
+            if ~obj.Hold
+                obj.Hide;
+            else
+                obj.DialogText = '';
+                obj.Update;
+            end
+        end
+        
+        function SetProgressText(obj, text)
+            if nargin < 2
+               text = 'Please wait'; 
+            end            
+            obj.DialogText = PTKTextUtilities.RemoveHtml(text);
+            obj.Update;
+            obj.ShowPanel;            
+        end
+        
+        function SetProgressValue(obj, progress_value)
+            obj.ProgressValue = progress_value;
+            obj.ShowProgressBar = true;
+            obj.Update;
+            obj.ShowPanel;
+        end
+        
+        function SetProgressAndMessage(obj, progress_value, text)
+            obj.ShowProgressBar = true;
+            obj.DialogText = PTKTextUtilities.RemoveHtml(text);
+            obj.ProgressValue = progress_value;
+            obj.Update;
+            obj.ShowPanel;            
+        end
+        
+        function cancelled = CancelClicked(obj)
+            cancelled = obj.UserClickedCancel;
+            obj.UserClickedCancel = false;
+        end
+        
+    end
+    
+    methods (Access = private)
+        function Update(obj)
+            if isempty(obj.LastTitle) || ~strcmp(obj.DialogTitle, obj.LastTitle)
+                set(obj.ProgressTitle, 'String', obj.DialogTitle);
+                obj.LastTitle = obj.DialogTitle;
+            end
+            
+            if isempty(obj.LastText) || ~strcmp(obj.DialogText, obj.LastText)
+                set(obj.Text, 'String', obj.DialogText);
+                obj.LastText = obj.DialogText;
+            end
+            
+            obj.ProgressBarHandle.setValue(obj.ProgressValue);
+            
+            if isempty(obj.TimerRef) || toc(obj.TimerRef) > obj.MaxTimeBetweenUpdates
+                obj.TimerRef = tic;
+                drawnow;
+            end
+        end
+        
+        function ShowPanel(obj)
+            if obj.Disabled || obj.PanelIsShown
+                return;
+            end            
+            set(obj.Text, 'Visible', 'on');
+            set(obj.ProgressTitle, 'Visible', 'on');
+
+            if PTKSoftwareInfo.DebugMode
+                set(obj.Quit, 'Visible', 'on');
+            end
+
+            set(obj.Cancel, 'Visible', 'on');
+            set(obj.ProgressBarHandle, 'visible', 1);
+            
+            obj.PanelIsShown = true;            
+        end
+        
+        function HidePanel(obj)
+            
+            if obj.Disabled || ~obj.PanelIsShown
+                return;
+            end
+            
+            set(obj.Text, 'Visible', 'off');
+            set(obj.ProgressTitle, 'Visible', 'off');
+            set(obj.Quit, 'Visible', 'off');
+            set(obj.Cancel, 'Visible', 'off');
+            set(obj.ProgressBarHandle, 'visible', 0);
+            obj.PanelIsShown = false;
+        end
+        
+        function CancelButton(obj, ~, ~)
+            obj.UserClickedCancel = true;
+        end
+        
+        function QuitButton(obj, ~, ~)
+            obj.Hide;
+            throw(MException('PTKCustomProgressDialog:UserForceQuit', 'User forced plugin to terminate'));
+        end
+    end
+    
+end
