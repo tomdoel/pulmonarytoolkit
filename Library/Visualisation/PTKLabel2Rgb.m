f64545df951880460c3bc5192dc9954c2aa60fa8
Tom Doel <pulmonarytoolkit@gmail.com>
2015-09-24 18:19:59 +0100

    Fix for label image generation to set zero values to black

--- Library/Visualisation/PTKLabel2Rgb.m
+++ Library/Visualisation/PTKLabel2Rgb.m
@@ -10,6 +10,8 @@ function rgb_image = PTKLabel2Rgb(label_image)
     %     Distributed under the GNU GPL v3 licence. Please see website for details.
     %
 
-    label_image = 1 + mod(label_image - 1, 63);    
-    rgb_image = label2rgb(label_image, PTKSoftwareInfo.Colormap);
+    adjusted_colourmap = [[0,0,0]; PTKSoftwareInfo.Colormap];
+    label_image_adjusted = 2 + mod(label_image - 1, 63);
+    label_image_adjusted(label_image == 0) = 1;
+    rgb_image = label2rgb(label_image_adjusted, adjusted_colourmap);
 end
