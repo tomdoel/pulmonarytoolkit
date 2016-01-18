f032a096bf19ca469e042f23e788a1139ad10b0d
Tom Doel <tomdoel@users.noreply.github.com>
2016-01-18 19:05:59 +0000

    Add an optional border correction parameter to Gaussian filtering

--- Library/PTKGaussianFilter.m
+++ Library/PTKGaussianFilter.m
@@ -1,4 +1,4 @@
-function filtered_image = PTKGaussianFilter(original_image, filter_size_mm)
+function filtered_image = PTKGaussianFilter(original_image, filter_size_mm, border_correction)
     % PTKGaussianFilter. Performs 3D Gaussian filtering on a 3D image.
     %
     %     PTKGaussianFilter takes in an image in a PTKImage class and performs 3D
@@ -15,7 +15,10 @@ function filtered_image = PTKGaussianFilter(original_image, filter_size_mm)
     %     Distributed under the GNU GPL v3 licence. Please see website for details.
     %
     
-
+    if nargin < 3
+        border_correction = false;
+    end
+    
     if ~isa(original_image, 'PTKImage')
         error('PTKGaussianFilter requires a PTKImage as input');
     end
@@ -25,7 +28,8 @@ function filtered_image = PTKGaussianFilter(original_image, filter_size_mm)
     
     epsilon = 1e-3;
     sigma_voxels = sigma_mm./voxel_size_mm;
-    hsize = 2*max(ceil((sigma_voxels).*sqrt(-2*log(sqrt(2*pi).*(sigma_voxels)*epsilon)))) + 1;
+    border_region = max(ceil((sigma_voxels).*sqrt(-2*log(sqrt(2*pi).*(sigma_voxels)*epsilon))));
+    hsize = 2*border_region + 1;
     n = 1 : hsize;
     center = hsize/2 + 0.5;
     
@@ -56,6 +60,15 @@ function filtered_image = PTKGaussianFilter(original_image, filter_size_mm)
     raw_image = raw_image - intensity_offset;
     raw_image = convn(convn(convn(raw_image, shiftdim(keri, 2), 'same'), shiftdim(kerj, 1), 'same'), kerk, 'same');
     raw_image = raw_image + intensity_offset;
+    
+    if border_correction
+        raw_image(1:border_region, :, :) = original_image.RawImage(1:border_region, :, :);
+        raw_image(end-border_region+1:end, :, :) = original_image.RawImage(end-border_region+1:end, :, :);
+        raw_image(:, 1:border_region, :) = original_image.RawImage(:, 1:border_region, :);
+        raw_image(:, end-border_region+1:end, :) = original_image.RawImage(:, end-border_region+1:end, :);
+        raw_image(:, :, 1:border_region) = original_image.RawImage(:, :, 1:border_region);
+        raw_image(:, :, end-border_region+1:end) = original_image.RawImage(:, :, end-border_region+1:end);
+    end
     filtered_image.ChangeRawImage(raw_image);
 end
 
