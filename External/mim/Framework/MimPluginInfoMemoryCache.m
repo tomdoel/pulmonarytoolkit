3d681db381ed2102b27ab42c1e658f491a691b11
Tom Doel <tomdoel@users.noreply.github.com>
2016-04-17 11:53:57 +0100

    Renamed local variable

--- External/mim/Framework/MimPluginInfoMemoryCache.m
+++ External/mim/Framework/MimPluginInfoMemoryCache.m
@@ -27,42 +27,42 @@ classdef MimPluginInfoMemoryCache < handle
         end
         
         function plugin_info = GetPluginInfo(obj, plugin_name, category, reporting)
-            [is_ptk_plugin, plugin_class] = obj.PopulateCache(plugin_name, category, reporting);
+            [is_mim_plugin, plugin_class] = obj.PopulateCache(plugin_name, category, reporting);
             if isempty(plugin_class)
                 reporting.Error('MimPluginInfoMemoryCache:PluginNotFound', ['The plugin ' plugin_name ' was not found. Please ensure this is a PTKPlugin class and it is in the path.']);
             end
-            if ~is_ptk_plugin
+            if ~is_mim_plugin
                 reporting.Error('MimPluginInfoMemoryCache:NotAPlugin', ['A file ' plugin_name ' was found but is not a valid PTKPlugin class. Please ensure this is a PTKPlugin class.']);
             end
             plugin_info = obj.PluginInfoMap(plugin_name);
         end
         
         function plugin_handle = GetPluginHandle(obj, plugin_name, category, reporting)
-            [is_ptk_plugin, plugin_class] = obj.PopulateCache(plugin_name, category, reporting);
+            [is_mim_plugin, plugin_class] = obj.PopulateCache(plugin_name, category, reporting);
             if isempty(plugin_class)
                 reporting.Error('MimPluginInfoMemoryCache:PluginNotFound', ['The plugin ' plugin_name ' was not found. Please ensure this is a PTKPlugin class and it is in the path.']);
             end
-            if ~is_ptk_plugin
+            if ~is_mim_plugin
                 reporting.Error('MimPluginInfoMemoryCache:NotAPlugin', ['A file ' plugin_name ' was found but is not a valid PTKPlugin class. Please ensure this is a PTKPlugin class.']);
             end
             plugin_handle = obj.PluginHandleMap(plugin_name);
         end
         
-        function [is_ptk_plugin, plugin_class_object] = IsPlugin(obj, plugin_name, category, reporting)
-            [is_ptk_plugin, plugin_class_object] = obj.PopulateCache(plugin_name, category, reporting);
+        function [is_mim_plugin, plugin_class_object] = IsPlugin(obj, plugin_name, category, reporting)
+            [is_mim_plugin, plugin_class_object] = obj.PopulateCache(plugin_name, category, reporting);
         end
     end
     
     methods (Access = private)
-        function [is_ptk_plugin, plugin_class] = PopulateCache(obj, plugin_name, category, reporting)
+        function [is_mim_plugin, plugin_class] = PopulateCache(obj, plugin_name, category, reporting)
             if obj.PluginHandleMap.isKey(plugin_name) && obj.PluginInfoMap.isKey(plugin_name)
-                is_ptk_plugin = true;
+                is_mim_plugin = true;
                 plugin_class = obj.PluginHandleMap(plugin_name);
                 return;
             end
             
             if (exist(plugin_name, 'class') ~= 8)
-                is_ptk_plugin = false;
+                is_mim_plugin = false;
                 plugin_class = [];
                 return;
             end
@@ -71,11 +71,11 @@ classdef MimPluginInfoMemoryCache < handle
             plugin_class = feval(plugin_handle);
 
             if ~isa(plugin_class, 'PTKPlugin')
-                is_ptk_plugin = false;
+                is_mim_plugin = false;
                 return;
             end
 
-            is_ptk_plugin = true;
+            is_mim_plugin = true;
             obj.PluginHandleMap(plugin_name) = plugin_class;
 
             % Parse the class properties into a data structure
