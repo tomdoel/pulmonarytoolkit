f2003004fd37f76515ad518a21794759c6971103
Tom Doel <tomdoel@users.noreply.github.com>
2016-10-08 18:06:37 +0100

    Update version number to 0.6.3

--- Framework/PTKSoftwareInfo.m
+++ Framework/PTKSoftwareInfo.m
@@ -18,7 +18,7 @@ classdef PTKSoftwareInfo < handle
     properties (Constant)
         
         % Version numbers
-        Version = '0.6.2'
+        Version = '0.6.3'
         DicomVersion = '0.1'
         DiskCacheSchema = '0.1'
         PTKVersion = '2'
