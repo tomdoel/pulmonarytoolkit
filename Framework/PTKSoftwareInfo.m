f8db9d848bb42a99adc8f7a21e9e2bae8d23aea9
Tom Doel <tomdoel@users.noreply.github.com>
2016-10-18 17:47:37 +0100

    Updated version number

--- Framework/PTKSoftwareInfo.m
+++ Framework/PTKSoftwareInfo.m
@@ -18,7 +18,7 @@ classdef PTKSoftwareInfo < handle
     properties (Constant)
         
         % Version numbers
-        Version = '0.6.3'
+        Version = '0.6.4'
         DicomVersion = '0.1'
         DiskCacheSchema = '0.1'
         PTKVersion = '2'
