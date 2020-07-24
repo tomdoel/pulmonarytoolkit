511eefedaa111d8a918228a7087f9a2346dde6f3
ashkanpakzad <ashpakzad@gmail.com>
2020-07-24 12:59:20 +0100

    New function based on MimSaveAsNifti.m for saving PTKImage as nifti image if it originates from a nifti image. This function saves the new image in the same orientation as the original.

---
+++ External/mim/Library/File/MimSaveNiftiAsNifti.m
@@ -0,0 +1,50 @@
+function MimSaveNiftiAsNifti(image_to_save, path, filename, reporting)
+    % MimSaveAsNifti. Writes out a PTKImage in NIFTI format if originally
+    % loaded from NIFTI format using PTK.
+    %
+    %     Syntax
+    %     ------
+    %
+    %         MimSaveNiftiAsNifti(image_data, path, filename, data_type, orientation, reporting)
+    %
+    %             image_to_save   is a PTKImage (or PTKDicomImage) class containing the image
+    %                             to be saved
+    %             path, filename  specify the location to save the NIFTI data.
+    %             reporting       an object implementing CoreReportingInterface
+    %                             for reporting progress and warnings
+    %
+    %
+    %     Licence
+    %     -------
+    %     Part of the TD MIM Toolkit. https://github.com/tomdoel
+    %     Author: Tom Doel, Copyright Tom Doel 2014.  www.tomdoel.com
+    %     Author: Ashkan Pakzad, ashkanpakzad.github.io
+    %     Distributed under the MIT licence. Please see website for details.
+    %        
+    
+    if nargin < 4
+        reporting = CoreReportingDefault();
+    end
+    
+    if ~isa(image_to_save, 'PTKImage')
+        reporting.Error('MimSaveAsNifti:InputMustBePTKImage', 'Requires a PTKImage as input');
+    end
+
+    image_data = image_to_save.RawImage;
+    
+    full_filename = fullfile(path, filename);
+
+    % x y switch
+    resolution = image_to_save.VoxelSize([2, 1, 3]);
+    image_data = permute(image_data, [2, 1, 3]);
+    
+    % populate header class to with required accurate fields
+    metadata = image_to_save.MetaHeader;
+    metadata.Filename = full_filename;
+    metadata.ImageSize = size(image_data);
+    metadata.PixelDimensions = resolution;
+    metadata.Datatype = class(image_data);
+    
+    niftiwrite(image_data, full_filename, metadata);
+end
+
