37744542319648f33f1eaddc47616eee84cb4d61
Tom Doel <tomdoel@users.noreply.github.com>
2023-06-28 14:45:52 +0100

    Whitespace changes

--- External/mim/Gui/Panels/MimSidePanel.m
+++ External/mim/Gui/Panels/MimSidePanel.m
@@ -12,8 +12,8 @@ classdef MimSidePanel < GemPanel
     %     Part of the TD MIM Toolkit. https://github.com/tomdoel
     %     Author: Tom Doel, Copyright Tom Doel 2014.  www.tomdoel.com
     %     Distributed under the MIT licence. Please see website for details.
-    %    
-    
+    %
+
     properties (Access = private)
         ProjectsSidePanel
         PatientsSidePanel
@@ -31,55 +31,55 @@ classdef MimSidePanel < GemPanel
         PatientNameFontSize = 40
         PatientNameHeight = 40
         PatientNameWidth = 200
-        PatientNameLeftPadding = 10        
+        PatientNameLeftPadding = 10
         BorderSpace = 10
         SpacingBetweenLists = 20
     end
-    
+
     methods
         function obj = MimSidePanel(parent, controller, patient_database, state, linked_recorder, group_patients_with_same_name)
             obj = obj@GemPanel(parent);
-            
+
             obj.RightBorder = true;
-            
+
             obj.Controller = controller;
             obj.GuiState = state;
-            
+
             obj.ProjectsSidePanel = MimProjectsSidePanel(obj, patient_database, controller);
             obj.AddChild(obj.ProjectsSidePanel);
-            
+
             obj.PatientsSidePanel = MimPatientsSidePanel(obj, patient_database, group_patients_with_same_name, controller);
             obj.AddChild(obj.PatientsSidePanel);
-            
+
             obj.LinkedSeriesSidePanel = MimLinkedSeriesSidePanel(obj, patient_database, linked_recorder, group_patients_with_same_name, controller);
             obj.AddChild(obj.LinkedSeriesSidePanel);
             obj.LinkedSeriesSidePanel.TopBorder = true;
-            
+
             obj.SeriesSidePanel = MimSeriesSidePanel(obj, patient_database, group_patients_with_same_name, controller);
             obj.AddChild(obj.SeriesSidePanel);
             obj.SeriesSidePanel.TopBorder = true;
-            
+
             obj.Repopulate;
 
             % Add listeners for changes to the loaded series
-            obj.AddEventListener(controller, 'ProjectChangedEvent', @obj.ProjectChanged);            
-            obj.AddEventListener(controller, 'PatientChangedEvent', @obj.PatientChanged);            
-            obj.AddEventListener(state, 'SeriesUidChangedEvent', @obj.SeriesChanged);            
+            obj.AddEventListener(controller, 'ProjectChangedEvent', @obj.ProjectChanged);
+            obj.AddEventListener(controller, 'PatientChangedEvent', @obj.PatientChanged);
+            obj.AddEventListener(state, 'SeriesUidChangedEvent', @obj.SeriesChanged);
             obj.AddEventListener(state, 'PatientIdChangedEvent', @obj.PatientChanged);
-            obj.AddEventListener(linked_recorder, 'LinkingChanged', @obj.LinkingChanged);            
+            obj.AddEventListener(linked_recorder, 'LinkingChanged', @obj.LinkingChanged);
             obj.AddEventListener(patient_database, 'DatabaseHasChanged', @obj.DatabaseHasChanged);
         end
-        
+
         function CreateGuiComponent(obj, position)
             CreateGuiComponent@GemPanel(obj, position);
         end
-        
+
         function Resize(obj, new_position)
             Resize@GemPanel(obj, new_position);
-            
+
             % After calling Resize@GemPanel, the position will have been adjusted due to the border
             new_position = obj.InnerPosition;
-            
+
             % Only show the projects panel if there is more than one
             % project
             if obj.ProjectsSidePanel.GetNumItems < 2
@@ -99,14 +99,14 @@ classdef MimSidePanel < GemPanel
                 obj.LinkedSeriesSidePanel.Enable;
                 panels = [panels, {obj.PatientsSidePanel, obj.LinkedSeriesSidePanel, obj.SeriesSidePanel}];
             end
-            
+
             available_height = new_position(4) - (numel(panels) - 1)*obj.SpacingBetweenLists;
-            
+
             panel_heights = obj.GetPanelHeights(panels, new_position(3), available_height);
 
             y_offset = max(0, available_height - sum(panel_heights));
             y_position = new_position(2) + y_offset;
-            
+
             for panel_index = numel(panels) : -1 : 1
                 panel = panels{panel_index};
                 panel_position = new_position;
@@ -116,12 +116,12 @@ classdef MimSidePanel < GemPanel
                 y_position = y_position + obj.SpacingBetweenLists + panel_heights(panel_index);
             end
         end
-        
-        
+
+
         function height = GetRequestedHeight(obj, width)
             height = obj.PanelHeight;
         end
-        
+
         function Repopulate(obj)
             obj.ProjectsSidePanel.RepopulateSidePanel(obj.Controller.GetCurrentProject);
             obj.PatientsSidePanel.RepopulateSidePanel(obj.GuiState.CurrentPatientId);
@@ -135,7 +135,7 @@ classdef MimSidePanel < GemPanel
         function Refresh(obj, ~, ~)
             obj.Repopulate;
         end
-        
+
         function input_has_been_processed = ShortcutKeys(obj, key)
             % Process shortcut keys for the side panel.
             if strcmpi(key, 'end')
@@ -162,18 +162,18 @@ classdef MimSidePanel < GemPanel
             else
                 input_has_been_processed = false;
             end
-        end        
+        end
     end
-    
+
     methods (Access = private)
         function DatabaseHasChanged(obj, ~, ~)
             obj.Repopulate;
         end
-        
+
         function ProjectChanged(obj, ~, event_data)
             project_id = event_data.Data;
             project_has_changed = obj.ProjectsSidePanel.UpdateSidePanel(project_id);
-            
+
             if project_has_changed
                 obj.PatientsSidePanel.RepopulateSidePanel(obj.GuiState.CurrentPatientId);
                 obj.LinkedSeriesSidePanel.RepopulateSidePanel(obj.PatientsSidePanel.CurrentPatientId, obj.GuiState.CurrentSeriesUid);
@@ -183,11 +183,11 @@ classdef MimSidePanel < GemPanel
                 end
             end
         end
-        
+
         function PatientChanged(obj, ~, event_data)
             patient_id = event_data.Data;
             patient_has_changed = obj.PatientsSidePanel.UpdateSidePanel(patient_id);
-            
+
             if patient_has_changed
                 obj.LinkedSeriesSidePanel.RepopulateSidePanel(patient_id, obj.GuiState.CurrentSeriesUid);
                 obj.SeriesSidePanel.RepopulateSidePanel(patient_id, obj.GuiState.CurrentSeriesUid);
@@ -196,12 +196,12 @@ classdef MimSidePanel < GemPanel
                 end
             end
         end
-        
+
         function SeriesChanged(obj, ~, ~)
             % This event fires when the loaded series has been changed.
-            
+
             patient_has_changed = obj.PatientsSidePanel.UpdateSidePanel(obj.GuiState.CurrentPatientId);
-            
+
             % If the patient has changed, we need to repopulate the series list and change
             % the highlighted patient
             if patient_has_changed
@@ -212,11 +212,11 @@ classdef MimSidePanel < GemPanel
                 end
             else
                 % Otherwise, we only need to change the highlighted series
-                obj.LinkedSeriesSidePanel.UpdateSidePanel(obj.GuiState.CurrentSeriesUid);                
-                obj.SeriesSidePanel.UpdateSidePanel(obj.GuiState.CurrentSeriesUid);                
+                obj.LinkedSeriesSidePanel.UpdateSidePanel(obj.GuiState.CurrentSeriesUid);
+                obj.SeriesSidePanel.UpdateSidePanel(obj.GuiState.CurrentSeriesUid);
             end
         end
-        
+
         function LinkingChanged(obj, secondary_uid, ~)
             obj.LinkedSeriesSidePanel.RepopulateSidePanel(obj.PatientsSidePanel.CurrentPatientId, obj.GuiState.CurrentSeriesUid);
             if obj.ComponentHasBeenCreated
@@ -224,7 +224,7 @@ classdef MimSidePanel < GemPanel
             end
         end
     end
-    
+
     methods (Static, Access = private)
         function panel_heights = GetPanelHeights(panels, width, total_height)
             num_panels = numel(panels);
@@ -234,7 +234,7 @@ classdef MimSidePanel < GemPanel
             end
             panel_heights = zeros(size(requested_heights));
             [sorted_heights, sorted_heights_indices] = sort(requested_heights, 'ascend');
-            
+
             height_remaining = total_height;
             for sorted_panel_index = 1 : num_panels
                 remaining_panels = num_panels - sorted_panel_index + 1;
@@ -247,7 +247,7 @@ classdef MimSidePanel < GemPanel
                 panel_heights(sorted_heights_indices(sorted_panel_index)) = panel_height;
                 height_remaining = height_remaining - panel_height;
             end
-            
-        end        
+
+        end
     end
 end
